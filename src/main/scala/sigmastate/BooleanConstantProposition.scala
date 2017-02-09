89aa9f027a9c629f73a40f3190e15b0202ce967d
Alex Chepurnoy <alex.chepurnoy@iohk.io>
2017-02-09 12:09:02 +0300

    value added to BooleanConstantProposition

--- src/main/scala/sigmastate/BooleanConstantProposition.scala
+++ src/main/scala/sigmastate/BooleanConstantProposition.scala
@@ -1,7 +1,9 @@
 package sigmastate
 
 
-sealed trait BooleanConstantProposition extends SigmaStateProposition
+sealed trait BooleanConstantProposition extends SigmaStateProposition {
+  val value: Boolean
+}
 
 object BooleanConstantProposition {
   def fromBoolean(b: Boolean) = b match {
@@ -12,9 +14,11 @@ object BooleanConstantProposition {
 
 
 case object TrueProposition extends BooleanConstantProposition {
+  override val value = true
   override lazy val bytes: Array[Byte] = ???
 }
 
 case object FalseProposition extends BooleanConstantProposition {
+  override val value = false
   override lazy val bytes: Array[Byte] = ???
 }
