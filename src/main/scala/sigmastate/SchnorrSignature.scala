0d2e171a0b1e379ee2560ba4316067f628aea00f
Alex Chepurnoy <alex.chepurnoy@iohk.io>
2017-03-27 18:18:39 +0300

    code cleaning from the commented out code

--- src/main/scala/sigmastate/SchnorrSignature.scala
+++ src/main/scala/sigmastate/SchnorrSignature.scala
@@ -1,8 +1,7 @@
 package sigmastate
 
-import scorex.core.serialization.Serializer
 import edu.biu.scapi.primitives.dlog.bc.BcDlogECFp
-import edu.biu.scapi.primitives.dlog.{DlogGroup, ECElementSendableData}
+import edu.biu.scapi.primitives.dlog.DlogGroup
 import scapi.sigma.rework.{Challenge, NonInteractiveProver}
 import scapi.sigma.rework.DLogProtocol._
 import scorex.crypto.hash.Blake2b256
@@ -10,33 +9,6 @@ import scorex.crypto.hash.Blake2b256
 
 // TODO: make implementation corresponding to RFC-8032 standard for EdDSA signatures
 
-/*
-case class SchnorrSignature(signature: Array[Byte])(implicit val dlogGroup: DlogGroup)
-  extends ProofOfKnowledge[DLogSigmaProtocol, DLogCommonInput] {
-
-  import SchnorrSignature._
-
-  override def verify(proposition: DLogCommonInput, message: ProofOfKnowledge.Challenge): Boolean = {
-    //signature is g^r as a pair of points, and z
-    val (grx, gry, zb) = EcPointFunctions.decodeBigIntTriple(signature).get
-    val gr = new ECElementSendableData(grx, gry)
-
-    //h = g^w is a pubkey
-    val x: DLogCommonInput = proposition
-
-    val a: FirstDLogProverMessage = FirstDLogProverMessage(gr)
-
-    val z: SecondDLogProverMessage = SecondDLogProverMessage(zb)
-
-    val sigmaTranscript = DLogTranscript(x, a, Challenge(hf(message)), z)
-    sigmaTranscript.accepted
-  }
-
-  override val propCode: SigmaProposition.PropositionCode = 0: Byte
-  override type M = this.type
-
-  override def serializer: Serializer[SchnorrSignature.this.type] = ???
-}*/
 
 object SchnorrSignature {
   implicit val soundness = 256
@@ -59,7 +31,6 @@ case class SchnorrSignatureSigner(privateInput: DLogProverInput)
   }
 
   def prove(challenge: Array[Byte]): SchnorrNode = {
-
     val g = dlog.getGenerator
     val gw = dlog.exponentiate(g, privateInput.w)
     val commonInput = DLogNode(gw)
@@ -83,5 +54,3 @@ case class SchnorrSignatureSigner(privateInput: DLogProverInput)
 
   override val publicInput: DLogNode = proposition
 }
-
-
