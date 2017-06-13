5122e78c25fb993d358b304bc2aff107ec7ac748
Alex Chepurnoy <alex.chepurnoy@iohk.io>
2017-06-13 13:49:41 +0300

    TreeConversion removed

--- src/main/scala/sigmastate/interpreter/DLogProverInterpreter.scala
+++ src/main/scala/sigmastate/interpreter/DLogProverInterpreter.scala
@@ -1,10 +1,7 @@
 package sigmastate.interpreter
 
 import scapi.sigma.rework.DLogProtocol
-import scapi.sigma.rework.DLogProtocol.DLogNode
 import sigmastate._
-import sigmastate.utils.Helpers
-import org.bitbucket.inkytonik.kiama.attribution.Attribution
 import org.bitbucket.inkytonik.kiama.rewriting.Rewriter.{everywherebu, everywheretd, rule}
 import org.bitbucket.inkytonik.kiama.rewriting.Strategy
 
@@ -46,38 +43,5 @@ trait DLogProverInterpreter extends ProverInterpreter {
     everywheretd(challengeDisperse)(t).get.asInstanceOf[UnprovenTree]
   }
 
-  override def prove(unproven: UnprovenTree): ProofT = TreeConversion.proving(secrets)(unproven)
-}
-
-
-
-object TreeConversion extends Attribution {
-
-  //to be applied bottom up, converts SigmaTree => UnprovenTree
-  val convertToUnproven: SigmaTree => UnprovenTree = attr {
-    case CAND(sigmaTrees) => CAndUnproven(CAND(sigmaTrees), None, sigmaTrees.map(convertToUnproven))
-    case COR2(left, right) => COr2Unproven(COR2(left, right), None, convertToUnproven(left), convertToUnproven(right))
-    case ci: DLogNode => SchnorrUnproven(None, simulated = false, ci)
-  }
-
-  val proving: Seq[DLogProtocol.DLogProverInput] => UnprovenTree => UncheckedTree = paramAttr { secrets => {
-    case SchnorrUnproven(Some(challenge), simulated, proposition) =>
-      if (simulated) {
-        SchnorrSigner(proposition.asInstanceOf[DLogNode],None).prove(challenge)
-      } else {
-        val privKey = secrets.find(_.publicImage.h == proposition.h).get
-        SchnorrSigner.generate(privKey).prove(challenge)
-      }
-
-    case CAndUnproven(proposition, Some(challenge), children) =>
-      val proven = children.map(proving(secrets))
-      CAndUncheckedNode(proposition, challenge, proven)
-
-    case COr2Unproven(proposition, Some(challenge), leftChild, rightChild) =>
-      assert(Helpers.xor(leftChild.challengeOpt.get, rightChild.challengeOpt.get).sameElements(challenge))
-
-      COr2UncheckedNode(proposition, challenge, proving(secrets)(leftChild), proving(secrets)(rightChild))
-    case _ => ???
-  }
-  }
+  override def prove(unproven: UnprovenTree): ProofT = proving(secrets)(unproven)
 }
--- src/main/scala/sigmastate/interpreter/ProverInterpreter.scala
+++ src/main/scala/sigmastate/interpreter/ProverInterpreter.scala
@@ -1,8 +1,11 @@
 package sigmastate.interpreter
 
+import org.bitbucket.inkytonik.kiama.attribution.AttributionCore
 import org.bitbucket.inkytonik.kiama.relation.Tree
+import scapi.sigma.rework.DLogProtocol
 import scapi.sigma.rework.DLogProtocol.DLogNode
 import sigmastate._
+import sigmastate.utils.Helpers
 
 import scala.util.Try
 
@@ -11,7 +14,7 @@ import scala.util.Try
   */
 case class ProverResult[ProofT <: UncheckedTree](proof: ProofT, extension: ContextExtension)
 
-trait ProverInterpreter extends Interpreter {
+trait ProverInterpreter extends Interpreter with AttributionCore {
 
   val contextExtenders: Map[Int, ByteArrayLeaf]
 
@@ -55,9 +58,36 @@ trait ProverInterpreter extends Interpreter {
           case FalseConstantNode => ???
         }
       case _ =>
-        val ct = TreeConversion.convertToUnproven(cProp.asInstanceOf[SigmaT]).withChallenge(challenge)
+        val ct = convertToUnproven(cProp.asInstanceOf[SigmaT]).withChallenge(challenge)
         val toProve = normalizeUnprovenTree(ct)
         prove(toProve)
     }, ext)
   }
+
+  //to be applied bottom up, converts SigmaTree => UnprovenTree
+  val convertToUnproven: SigmaTree => UnprovenTree = attr {
+    case CAND(sigmaTrees) => CAndUnproven(CAND(sigmaTrees), None, sigmaTrees.map(convertToUnproven))
+    case COR2(left, right) => COr2Unproven(COR2(left, right), None, convertToUnproven(left), convertToUnproven(right))
+    case ci: DLogNode => SchnorrUnproven(None, simulated = false, ci)
+  }
+
+  val proving: Seq[DLogProtocol.DLogProverInput] => UnprovenTree => UncheckedTree = paramAttr { secrets => {
+    case SchnorrUnproven(Some(challenge), simulated, proposition) =>
+      if (simulated) {
+        SchnorrSigner(proposition.asInstanceOf[DLogNode],None).prove(challenge)
+      } else {
+        val privKey = secrets.find(_.publicImage.h == proposition.h).get
+        SchnorrSigner.generate(privKey).prove(challenge)
+      }
+
+    case CAndUnproven(proposition, Some(challenge), children) =>
+      val proven = children.map(proving(secrets))
+      CAndUncheckedNode(proposition, challenge, proven)
+
+    case COr2Unproven(proposition, Some(challenge), leftChild, rightChild) =>
+      assert(Helpers.xor(leftChild.challengeOpt.get, rightChild.challengeOpt.get).sameElements(challenge))
+
+      COr2UncheckedNode(proposition, challenge, proving(secrets)(leftChild), proving(secrets)(rightChild))
+    case _ => ???
+  }}
 }
