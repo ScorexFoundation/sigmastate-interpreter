44fb6bc6b9946f5fae10c2b500a3fc969fa93b8e
Alex Chepurnoy <alex.chepurnoy@iohk.io>
2017-06-26 10:38:10 +0300

    UtxoProvingInterpreter pulled out in a separate file

--- src/test/scala/sigmastate/utxo/UtxoInterpreterSpecification.scala
+++ src/test/scala/sigmastate/utxo/UtxoInterpreterSpecification.scala
@@ -2,47 +2,11 @@ package sigmastate.utxo
 
 import org.scalatest.{Matchers, PropSpec}
 import org.scalatest.prop.{GeneratorDrivenPropertyChecks, PropertyChecks}
-import scapi.sigma.rework.DLogProtocol.{DLogNode, DLogProverInput}
+import scapi.sigma.rework.DLogProtocol.DLogNode
 import scorex.crypto.encode.Base16
 import scorex.crypto.hash.Blake2b256
-import scorex.utils.Random
 import sigmastate._
-import sigmastate.interpreter.ProverInterpreter
 import sigmastate.utils.Helpers
-
-
-class UtxoProvingInterpreter extends UtxoInterpreter with ProverInterpreter {
-
-  override lazy val secrets: Seq[DLogProverInput] = {
-    import SchnorrSignature._
-    (1 to 2).map(_ => DLogProverInput.random()._1)
-  }
-
-  override lazy val contextExtenders: Map[Int, ByteArrayLeaf] = (1 to 10).map { i =>
-    val ba = Random.randomBytes(75)
-    Helpers.tagInt(ba) -> ByteArrayLeaf(ba)
-  }.toMap
-
-  def withContextExtender(tag: Int, value: ByteArrayLeaf): UtxoProvingInterpreter = {
-    val s = secrets
-    val ce = contextExtenders
-
-    new UtxoProvingInterpreter {
-      override lazy val secrets: Seq[DLogProverInput] = s
-      override lazy val contextExtenders: Map[Int, ByteArrayLeaf] = ce + (tag -> value)
-    }
-  }
-
-  def withSecrets(additionalSecrets: Seq[DLogProverInput]) = {
-    val ce = contextExtenders
-    val s = secrets ++ additionalSecrets
-
-    new UtxoProvingInterpreter {
-      override lazy val secrets: Seq[DLogProverInput] = s
-      override lazy val contextExtenders: Map[Int, ByteArrayLeaf] = ce
-    }
-  }
-}
 
 
 class UtxoInterpreterSpecification extends PropSpec
---
+++ src/test/scala/sigmastate/utxo/UtxoProvingInterpreter.scala
@@ -0,0 +1,41 @@
+package sigmastate.utxo
+
+import scapi.sigma.rework.DLogProtocol.{DLogNode, DLogProverInput}
+import scorex.utils.Random
+import sigmastate.ByteArrayLeaf
+import sigmastate.interpreter.ProverInterpreter
+import sigmastate.utils.Helpers
+
+class UtxoProvingInterpreter extends UtxoInterpreter with ProverInterpreter {
+  implicit val dlog = DLogNode.dlogGroup
+  implicit val soundness: Int = 256
+
+  override lazy val secrets: Seq[DLogProverInput] = {
+    (1 to 2).map(_ => DLogProverInput.random()._1)
+  }
+
+  override lazy val contextExtenders: Map[Int, ByteArrayLeaf] = (1 to 10).map { i =>
+    val ba = Random.randomBytes(75)
+    Helpers.tagInt(ba) -> ByteArrayLeaf(ba)
+  }.toMap
+
+  def withContextExtender(tag: Int, value: ByteArrayLeaf): UtxoProvingInterpreter = {
+    val s = secrets
+    val ce = contextExtenders
+
+    new UtxoProvingInterpreter {
+      override lazy val secrets: Seq[DLogProverInput] = s
+      override lazy val contextExtenders: Map[Int, ByteArrayLeaf] = ce + (tag -> value)
+    }
+  }
+
+  def withSecrets(additionalSecrets: Seq[DLogProverInput]): UtxoProvingInterpreter = {
+    val ce = contextExtenders
+    val s = secrets ++ additionalSecrets
+
+    new UtxoProvingInterpreter {
+      override lazy val secrets: Seq[DLogProverInput] = s
+      override lazy val contextExtenders: Map[Int, ByteArrayLeaf] = ce
+    }
+  }
+}
