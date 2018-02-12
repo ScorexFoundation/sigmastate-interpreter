eb82fc79ce58fd221ef9ee80a95cc9a58bc84fbf
Alex Chepurnoy <alex.chepurnoy@iohk.io>
2018-02-12 15:06:42 +0300

    boxWithMetadata simplification

--- src/test/scala/sigmastate/utxo/BoxHelpers.scala
+++ src/test/scala/sigmastate/utxo/BoxHelpers.scala
@@ -4,10 +4,10 @@ import scorex.crypto.hash.Blake2b256
 import sigmastate.{SigmaStateTree, TrueLeaf}
 
 object BoxHelpers {
-  def boxWithMetadata(value: Int, proposition: SigmaStateTree, creationHeight: Int = 0, boxIndex: Short = 0) =
+  def boxWithMetadata(value: Int, proposition: SigmaStateTree, creationHeight: Int = 0) =
     BoxWithMetadata(SigmaStateBox(value, proposition), BoxMetadata(creationHeight))
 
-  val fakeSelf = boxWithMetadata(0, TrueLeaf)
+  val fakeSelf: BoxWithMetadata = boxWithMetadata(0, TrueLeaf)
 
   //fake message, in a real-life a message is to be derived from a spending transaction
   val fakeMessage = Blake2b256("Hello World")
