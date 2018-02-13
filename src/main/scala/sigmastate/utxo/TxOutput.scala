2ed5f32389d569a3c7b19e26a26fa0c7eff4d821
Alex Chepurnoy <alex.chepurnoy@iohk.io>
2018-02-13 15:12:13 +0300

    TxOutput eliminated

--- src/main/scala/sigmastate/utxo/TxOutput.scala
+++
@@ -1,9 +0,0 @@
-package sigmastate.utxo
-
-import sigmastate.NotReadyValueBox
-import sigmastate.utxo.CostTable.Cost
-
-//todo: replace with ByIndex on Collection
-case class TxOutput(outIndex: Int) extends NotReadyValueBox {
-  override val cost: Int = Cost.TxOutputDeclaration
-}
