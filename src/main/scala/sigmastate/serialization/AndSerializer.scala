059f42d602b70ff9f66fc343201a52a04e1ec035
Alex Chepurnoy <alex.chepurnoy@iohk.io>
2018-04-13 12:25:25 +0300

    typing todo

--- src/main/scala/sigmastate/serialization/AndSerializer.scala
+++ src/main/scala/sigmastate/serialization/AndSerializer.scala
@@ -10,6 +10,8 @@ object AndSerializer extends ValueSerializer[AND] {
   override val opCode = ValueSerializer.AndCode
   val typeCode: TypeCode = SBoolean.typeCode
 
+  //todo: should we check typing rules here? i.e. the fact that collection indeed contains booleans
+  // todo: (it is not checked by Scala)
   override def parseBody(bytes: Array[Byte], pos: Position) = {
     val (body, consumed) = ValueSerializer.deserialize(bytes, pos)
     new AND(body.asInstanceOf[Value[SCollection[SBoolean.type]]]) -> consumed
