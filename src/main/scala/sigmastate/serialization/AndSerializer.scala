937cabed5737121994ea99a2aff592216f09014b
Alex Chepurnoy <alex.chepurnoy@iohk.io>
2018-04-13 12:18:31 +0300

    AndSerializer fix

--- src/main/scala/sigmastate/serialization/AndSerializer.scala
+++ src/main/scala/sigmastate/serialization/AndSerializer.scala
@@ -1,6 +1,7 @@
 package sigmastate.serialization
 
 import sigmastate.SType.TypeCode
+import sigmastate.Values.Value
 import sigmastate._
 import sigmastate.serialization.ValueSerializer.Position
 
@@ -9,8 +10,10 @@ object AndSerializer extends ValueSerializer[AND] {
   override val opCode = ValueSerializer.AndCode
   val typeCode: TypeCode = SBoolean.typeCode
 
-  override def parseBody(bytes: Array[Byte], pos: Position) = ValueSerializer.deserialize(bytes, pos)
-
+  override def parseBody(bytes: Array[Byte], pos: Position) = {
+    val (body, consumed) = ValueSerializer.deserialize(bytes, pos)
+    new AND(body.asInstanceOf[Value[SCollection[SBoolean.type]]]) -> consumed
+  }
 
   override def serializeBody(and: AND): Array[TypeCode] = ValueSerializer.serialize(and.input)
 }
