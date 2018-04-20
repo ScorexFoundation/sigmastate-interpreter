90bc82d5d92525e607757a4f866f0a1f7be221f2
Dmitry Dyachkov <dimos-d@yandex.ru>
2018-04-21 02:33:45 +0300

    Added missing type Tag = OpCode

--- src/main/scala/sigmastate/serialization/ValueSerializer.scala
+++ src/main/scala/sigmastate/serialization/ValueSerializer.scala
@@ -21,6 +21,8 @@ trait ValueSerializer[V <: Value[SType]] extends SigmaSerializer[Value[SType], V
 object ValueSerializer
   extends SigmaSerializerCompanion[Value[SType]] {
 
+  type Tag = OpCode
+
   val table: Map[OpCode, ValueSerializer[_ <: Value[SType]]] = Seq[ValueSerializer[_ <: Value[SType]]](
 
     RelationSerializer(GtCode, GT.apply, Seq(Constraints.onlyInt2)),
