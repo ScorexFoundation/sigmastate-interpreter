d92d8bf819e865332c8e759e7e2fcabcfce8c5ff
Alex Chepurnoy <alex.chepurnoy@iohk.io>
2018-04-24 11:42:07 +0300

    TwoArgumentSerializerSpecification todo improved

--- src/test/scala/sigmastate/serialization/TwoArgumentSerializerSpecification.scala
+++ src/test/scala/sigmastate/serialization/TwoArgumentSerializerSpecification.scala
@@ -1,11 +1,14 @@
 package sigmastate.serialization
 
-import sigmastate.Values.{ByteArrayConstant, IntConstant}
+import java.math.BigInteger
+
+import sigmastate.Values.{BigIntConstant, ByteArrayConstant, GroupElementConstant, IntConstant}
 import sigmastate._
+import sigmastate.interpreter.GroupSettings
 
 class TwoArgumentSerializerSpecification extends TableSerializationSpecification {
 
-  // TODO: Yet remains to be done
+  private lazy val randomPoint = GroupSettings.dlogGroup.createRandomGenerator()
 
   override val objects =
     Table(
@@ -14,8 +17,10 @@ class TwoArgumentSerializerSpecification extends TableSerializationSpecification
       (Plus(IntConstant(2), IntConstant(3)), Array[Byte](41, 11, 0, 0, 0, 0, 0, 0, 0, 2, 11, 0, 0, 0, 0, 0, 0, 0, 3)),
       (Xor(ByteArrayConstant(Array(1, 2, 3)), ByteArrayConstant(Array(3, 4))), Array[Byte](42, 16, 0, 3, 1, 2, 3, 16, 0, 2, 3, 4)),
       (AppendBytes(ByteArrayConstant(Array(1, 2, 3)), ByteArrayConstant(Array(3, 4))), Array[Byte](43, 16, 0, 3, 1, 2, 3, 16, 0, 2, 3, 4)),
-      //(MultiplyGroup(GroupElementConstant(defaultPoint), GroupElementConstant(defaultPoint)), Array[Byte](25, 12, 13)),
-      //(Exponentiate(GroupElementConstant(Curve25519Point), BigIntConstant(BigInteger.valueOf(2.intValue()))), Array[Byte](26, 12, 13))
+
+      // TODO: uncomment and fix when a group will be considered
+      //(MultiplyGroup(GroupElementConstant(randomPoint), GroupElementConstant(randomPoint)), Array[Byte](25, 12, 13)),
+      //(Exponentiate(GroupElementConstant(randomPoint), BigIntConstant(BigInteger.valueOf(2.intValue()))), Array[Byte](26, 12, 13))
     )
 
   tableRoundTripTest("TwoArguments: serializer round trip")
