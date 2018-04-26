91559c82976cecd437f7c58a7339983be0bfb2dc
catena <catena@protonmail.com>
2018-04-26 17:38:46 +0300

    More complex MAST example

--- src/test/scala/sigmastate/utxo/examples/MASTExampleSpecification.scala
+++ src/test/scala/sigmastate/utxo/examples/MASTExampleSpecification.scala
@@ -11,6 +11,8 @@ import sigmastate.serialization.ValueSerializer
 import sigmastate.utxo.ErgoBox.R3
 import sigmastate.utxo._
 
+import scala.util.Random
+
 class MASTExampleSpecification extends SigmaTestingCommons {
 
   /**
@@ -20,18 +22,21 @@ class MASTExampleSpecification extends SigmaTestingCommons {
     * `provide a correct script and it's path to predefined tree root hash` into blockchain. When someone spend coins,
     * it provide a script, a Merkle path to script and required additional data. Thus, unsued branches of a script
     * remain unrevealed, providing more privacy and saving space in a blockchain.
-    *
     * See more at https://bitcointechtalk.com/what-is-a-bitcoin-merklized-abstract-syntax-tree-mast-33fdf2da5e2f
+    *
+    * In the provided example there are 5 different branches of a tree, each one require to reveal some secret.
+    *
     */
-  property("MAST") {
+  property("Merklized Abstract Syntax Tree") {
     val scriptId = 21.toByte
     val proofId = 22.toByte
     val secretId = 23.toByte
-    val allSecrets = (0 until 5).map(i => Blake2b256(i.toString + " blinding factor"))
-    val scriptBranches = allSecrets.map(s => OR(EQ(ByteArrayConstant(s), TaggedByteArray(secretId)), TrueLeaf))
+    val allSecrets = (0 until 5).map(i => Random.nextString(32).getBytes("UTF-8"))
+    val scriptBranches = allSecrets.map(s => EQ(ByteArrayConstant(s), TaggedByteArray(secretId)))
     val scriptBranchesBytes = scriptBranches.map(b => ValueSerializer.serialize(b))
     val treeElements = scriptBranchesBytes.map(s => (ADKey @@ Blake2b256(s), ADValue @@ s))
-    val (knownSecretTreeKey, knownSecret) = treeElements.head
+    val knownSecretTreeKey = treeElements.head._1
+    val knownSecret = ByteArrayConstant(allSecrets.head)
 
     val avlProver = new BatchAVLProver[Digest32, Blake2b256.type](keyLength = 32, None)
     treeElements.foreach(s => avlProver.performOneOperation(Insert(s._1, s._2)))
@@ -56,14 +61,11 @@ class MASTExampleSpecification extends SigmaTestingCommons {
     val knownSecretPathProof = avlProver.generateProof()
     val usedBranch = scriptBranchesBytes.head
     val prover = new ErgoProvingInterpreter()
-      .withContextExtender(secretId, ByteArrayConstant(knownSecret))
+      .withContextExtender(secretId, knownSecret)
       .withContextExtender(scriptId, ByteArrayConstant(usedBranch))
       .withContextExtender(proofId, ByteArrayConstant(knownSecretPathProof))
     val proof = prover.prove(prop, ctx, fakeMessage).get
 
     (new ErgoInterpreter).verify(prop, ctx, proof, fakeMessage).get shouldBe true
   }
-
-
-
 }
