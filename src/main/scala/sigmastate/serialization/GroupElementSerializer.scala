6ebe38eba3d680c7a3cbc687853dadacb3a27f5b
catena <catena@protonmail.com>
2018-05-07 16:18:49 +0300

    Comment points encoding

--- src/main/scala/sigmastate/serialization/GroupElementSerializer.scala
+++ src/main/scala/sigmastate/serialization/GroupElementSerializer.scala
@@ -14,12 +14,30 @@ object GroupElementSerializer extends ValueSerializer[GroupElementConstant] {
 
   override val opCode: OpCode = GroupElementConstantCode
 
+  /**
+    * Encode a point on this curve to a compressed ASN.1 encoding (X9.62 s 4.2.1 - 4.2.2).
+    *
+    * @return the point encoding
+    */
   override def serializeBody(gec: GroupElementConstant): Array[Byte] = gec.value.getEncoded(true)
 
-  override def parseBody(bytesIn: Array[Byte], pos: Int): (GroupElementConstant, Int) = {
-    val consumed = 1 + (curve.curve.getFieldSize + 7) / 8
-    val encoded = bytesIn.slice(pos, pos + consumed)
-    val point: SecP384R1Point = curve.curve.decodePoint(encoded).asInstanceOf[SecP384R1Point]
-    (GroupElementConstant(point), consumed)
+  /**
+    * Decode a point on this curve from its ASN.1 encoding.
+    * Only compressed points are supported (X9.62 s 4.2.1 pg 17).
+    *
+    * @return The decoded point.
+    */
+  override def parseBody(bytesIn: Array[Byte], pos: Int): (GroupElementConstant, Int) = bytesIn(pos) match {
+    case 0 =>
+      // infinity point is always compressed as 1 byte (X9.62 s 4.3.6)
+      val point = curve.curve.decodePoint(Array(bytesIn(pos))).asInstanceOf[SecP384R1Point]
+      (GroupElementConstant(point), 1)
+    case m if m == 2 || m == 3 =>
+      val consumed = 1 + (curve.curve.getFieldSize + 7) / 8
+      val encoded = bytesIn.slice(pos, pos + consumed)
+      val point = curve.curve.decodePoint(encoded).asInstanceOf[SecP384R1Point]
+      (GroupElementConstant(point), consumed)
+    case m =>
+      throw new Error(s"Only compressed encoding is supported, $m given")
   }
 }
