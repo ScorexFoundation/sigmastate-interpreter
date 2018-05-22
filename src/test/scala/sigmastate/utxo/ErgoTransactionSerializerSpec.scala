9012dfb3863cc01e52bd4949e538155a9eb7bceb
Denys Zadorozhnyi <denys@zadorozhnyi.com>
2018-05-22 17:18:03 +0300

    Show underlying exception with trace;

--- src/test/scala/sigmastate/utxo/ErgoTransactionSerializerSpec.scala
+++ src/test/scala/sigmastate/utxo/ErgoTransactionSerializerSpec.scala
@@ -4,8 +4,6 @@ import org.scalatest.PropSpec
 import org.scalatest.prop.GeneratorDrivenPropertyChecks
 import sigmastate.serialization.generators.ValueGenerators
 
-import scala.util.Success
-
 class ErgoTransactionSerializerSpec extends PropSpec
   with GeneratorDrivenPropertyChecks
   with SerializationRoundTripSpec
@@ -15,7 +13,7 @@ class ErgoTransactionSerializerSpec extends PropSpec
 
   property("ErgoTransaction: Serializer round trip") {
     forAll { t: ErgoTransaction =>
-      ergoTransactionSerializer.parseBytes(ergoTransactionSerializer.toBytes(t)) shouldEqual Success(t)
+      ergoTransactionSerializer.parseBytes(ergoTransactionSerializer.toBytes(t)).get shouldEqual t
     }
   }
 }
