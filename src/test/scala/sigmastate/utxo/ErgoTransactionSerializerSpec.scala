aac8d7108e97305a986304b9b18119bc1c43230f
Denys Zadorozhnyi <denys@zadorozhnyi.com>
2018-05-22 19:13:59 +0300

    Add `ErgoTransaction` deserialization test(red) for non-zero `pos` and `consumed`;

--- src/test/scala/sigmastate/utxo/ErgoTransactionSerializerSpec.scala
+++ src/test/scala/sigmastate/utxo/ErgoTransactionSerializerSpec.scala
@@ -1,5 +1,7 @@
 package sigmastate.utxo
 
+import org.scalacheck.Arbitrary._
+import org.scalacheck.Gen
 import org.scalatest.PropSpec
 import org.scalatest.prop.GeneratorDrivenPropertyChecks
 import sigmastate.serialization.generators.ValueGenerators
@@ -14,6 +16,15 @@ class ErgoTransactionSerializerSpec extends PropSpec
   property("ErgoTransaction: Serializer round trip") {
     forAll { t: ErgoTransaction =>
       ergoTransactionSerializer.parseBytes(ergoTransactionSerializer.toBytes(t)).get shouldEqual t
+    }
+  }
+
+  property("ErgoTransaction: start pos and consumed bytes") {
+    forAll { t: ErgoTransaction =>
+      val randomBytesCount = Gen.chooseNum(1, 20).sample.get
+      val randomBytes = Gen.listOfN(randomBytesCount, arbByte.arbitrary).sample.get.toArray
+      val bytes = ergoTransactionSerializer.toBytes(t)
+      ergoTransactionSerializer.parseBody(randomBytes ++ bytes, randomBytesCount) shouldEqual (t, bytes.length)
     }
   }
 }
