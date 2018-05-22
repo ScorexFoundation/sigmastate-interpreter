b3566be970accc06fba2f575d2ad34b0d54fcc68
Denys Zadorozhnyi <denys@zadorozhnyi.com>
2018-05-22 19:02:25 +0300

    Add `ErgoBoxCandidate` deserialization test(red) for non-zero `pos` and `consumed`;

--- src/test/scala/sigmastate/utxo/ErgoBoxCandidateSerializerSpec.scala
+++ src/test/scala/sigmastate/utxo/ErgoBoxCandidateSerializerSpec.scala
@@ -1,5 +1,7 @@
 package sigmastate.utxo
 
+import org.scalacheck.Arbitrary._
+import org.scalacheck.Gen
 import org.scalatest.PropSpec
 import org.scalatest.TryValues._
 import org.scalatest.prop.GeneratorDrivenPropertyChecks
@@ -19,6 +21,15 @@ class ErgoBoxCandidateSerializerSpec extends PropSpec
       b1.value shouldBe b.value
       b1.proposition shouldBe b.proposition
       b1.additionalRegisters shouldEqual b.additionalRegisters
+    }
+  }
+
+  property("ErgoBoxCandidate: start pos and consumed bytes") {
+    forAll { b: ErgoBoxCandidate =>
+      val randomBytesCount = Gen.chooseNum(1, 20).sample.get
+      val randomBytes = Gen.listOfN(randomBytesCount, arbByte.arbitrary).sample.get.toArray
+      val bytes = ergoBoxCandidateSerializer.toBytes(b)
+      ergoBoxCandidateSerializer.parseBody(randomBytes ++ bytes, randomBytesCount) shouldEqual (b, bytes.length)
     }
   }
 }
