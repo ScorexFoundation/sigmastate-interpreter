82ec9729654ed3a69442148139773d1a721ccb22
Ergo Morphic <ergomorphic@gmail.com>
2018-06-27 00:02:10 +0300

    more tests of building function Context => T

--- src/main/scala/sigmastate/lang/SigmaCoster.scala
+++ src/main/scala/sigmastate/lang/SigmaCoster.scala
@@ -1,9 +1,11 @@
 package sigmastate.lang
 
-import scalan.{SigmaLibrary}
+import org.ergoplatform.{Height, Outputs, Inputs}
+import scalan.SigmaLibrary
 import sigmastate._
 import sigmastate.Values.{Value, Constant}
 import sigmastate.serialization.OpCodes
+import sigmastate.utxo.SizeOf
 
 class SigmaCoster[Ctx <: SigmaLibrary](val ctx: Ctx) {
   import SigmaCoster._
@@ -30,21 +32,30 @@ class SigmaCoster[Ctx <: SigmaLibrary](val ctx: Ctx) {
     case _ => error(s"Cannot find BinOp for opcode $opCode")
   }
 
-  private def evalNode[T <: SType](node: Value[T]): Rep[T#WrappedType] = node match {
-    case Constant(v, tpe) => toRep(v)(stypeToElem(tpe))
-    case op: ArithOp[t] =>
-      val tpe = op.left.tpe
-      val et = stypeToElem(tpe)
-      val binop = opcodeToBinOp(op.opCode, et)
-      val x = evalNode(op.left)
-      val y = evalNode(op.right)
-      ApplyBinOp(binop, x, y)
-    case _ =>
-      error(s"Don't know how to evalNode($node)")
+  private def evalNode[T <: SType](ctx: Rep[Context], node: Value[T]): Rep[T#WrappedType] = {
+    val res: Rep[Any] = node match {
+      case Constant(v, tpe) => toRep(v)(stypeToElem(tpe))
+      case Height => ctx.HEIGHT
+      case Inputs => ctx.INPUTS
+      case Outputs => ctx.OUTPUTS
+      case SizeOf(xs) =>
+        val xs1 = evalNode(ctx, xs).asRep[Col[Any]]
+        xs1.length
+      case op: ArithOp[t] =>
+        val tpe = op.left.tpe
+        val et = stypeToElem(tpe)
+        val binop = opcodeToBinOp(op.opCode, et)
+        val x = evalNode(ctx, op.left)
+        val y = evalNode(ctx, op.right)
+        ApplyBinOp(binop, x, y)
+      case _ =>
+        error(s"Don't know how to evalNode($node)")
+    }
+    res.asRep[T#WrappedType]
   }
 
-  def buildCostedGraph[T <: SType](tree: Value[T]): Rep[T#WrappedType] = {
-    evalNode(tree)
+  def buildCostedGraph[T <: SType](tree: Value[T]): Rep[Context => T#WrappedType] = {
+    fun { ctx: Rep[Context] => evalNode(ctx, tree) }
   }
 }
 
--- src/test/scala/sigmastate/lang/SigmaCosterTest.scala
+++ src/test/scala/sigmastate/lang/SigmaCosterTest.scala
@@ -19,18 +19,28 @@ class SigmaCosterTest extends PropSpec with PropertyChecks with Matchers with La
     cg
   }
 
+  def cfun[T](f: Rep[Context] => Rep[T]) = fun(f)
+
+  def test[T](script: String, expectedFun: Rep[Context] => Rep[T]) = {
+    val x = cost(env, script)
+    val y = cfun(expectedFun)
+    x shouldBe y
+  }
+
   property("costed constants") {
-    cost(env, "1") should matchPattern { case Def(Const(1)) => }
-    cost(env, "1L") should matchPattern { case Def(Const(1L)) => }
+    test("1", _ => 1)
+    test("1L", _ => 1L)
   }
 
   property("costed operations") {
-    cost(env, "1 + 1") should matchPattern {
-      case Def(ApplyBinOp(op, Def(Const(1)), Def(Const(1)))) if op.opName == "+" =>
-    }
-    cost(env, "1L + 1L") should matchPattern {
-      case Def(ApplyBinOp(op, Def(Const(1L)), Def(Const(1L)))) if op.opName == "+" =>
-    }
+    test("1 + 1", _ => toRep(1) + toRep(1))
+    test("1L + 1L", _ => toRep(1L) + toRep(1L))
   }
+
+  property("costed context data") {
+    test("HEIGHT + 1L", ctx => ctx.HEIGHT + 1L)
+    test("INPUTS.size + OUTPUTS.size", ctx => ctx.INPUTS.length + ctx.OUTPUTS.length)
+  }
+
 
 }
