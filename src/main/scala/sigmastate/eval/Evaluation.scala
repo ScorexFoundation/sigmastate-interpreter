224d655621a3625441d05e0ab12807db36ea43ea
Ergo Morphic <ergomorphic@gmail.com>
2018-08-06 00:18:51 +0300

    graph Evaluation via reflection works for simple tests

--- src/main/scala/sigmastate/eval/Evaluation.scala
+++ src/main/scala/sigmastate/eval/Evaluation.scala
@@ -1,5 +1,7 @@
 package sigmastate.eval
 
+import java.lang.reflect.Method
+
 import sigmastate.Values.Value
 import sigmastate.lang.Costing
 
@@ -58,23 +60,77 @@ trait Evaluation extends Costing {
 
   type ContextFunc[T <: SType] = SigmaContext => Value[T]
 
-  def compile[T <: SType](f: Rep[Context => T#WrappedType]): ContextFunc[T] = {
-    val g = new PGraph(f)
-    val dataEnv = mutable.Map[Sym, Any]()
-    def out(s: Sym, v: Any) = dataEnv += (s -> v)
+  def compile[T <: SType](dataEnv: mutable.Map[Sym, AnyRef], f: Rep[Context => T#WrappedType]): ContextFunc[T] = {
 
-    def evaluate(te: TableEntry[_]): Unit = te.rhs match {
-      case Const(x) => out(te.sym, x)
-      case Lambda(l, _, x, y) =>
-        val f = (ctx: Any) => {
-          dataEnv += (x -> ctx)
-          l.schedule.foreach(evaluate(_))
-          dataEnv(y)
-        }
-        out(te.sym, f)
-      case _ => !!!(s"Don't know how to evaluate($te)")
+    def getArgTypes(args: Seq[AnyRef]) = {
+      val types = args.map {
+        case s: Sym => dataEnv(s).getClass
+        case _: Seq[_] => classOf[Seq[_]]
+      }
+      types
     }
-    g.schedule.foreach(te => evaluate(te))
+
+    def getArgValues(args: Seq[AnyRef]): Seq[AnyRef] = {
+      val vs = args.map {
+        case s: Sym => dataEnv(s)
+        case vec: Vector[AnyRef]@unchecked => Vector(getArgValues(vec):_*)
+      }
+      vs
+    }
+
+    def getObjMethod(objClass: Class[_], objMethod: Method, argTypes: Seq[Class[_]]): Method = {
+      val methods = objClass.getMethods
+      val lookupName = objMethod.getName
+      val resMethods = methods.filter(m => m.getName == lookupName)
+      def error = !!!(s"Cannot resolve of pre-staged method $objMethod in class $objClass")
+      resMethods.length match {
+        case 0 =>
+          error
+        case 1 =>
+          resMethods(0)
+        case _ =>
+          val res = resMethods.find { m =>
+            val mArgTypes = m.getParameterTypes
+            val N = mArgTypes.length
+            (N == argTypes.length) && {
+              (0 until N).forall { i =>
+                mArgTypes(i).isAssignableFrom(argTypes(i))
+              }
+            }
+          }
+          res.getOrElse(error)
+      }
+    }
+
+    def evaluate(te: TableEntry[_]): Unit = {
+      def out(v: AnyRef) = dataEnv += (te.sym -> v)
+      te.rhs match {
+        case Const(x) => out(x.asInstanceOf[AnyRef])
+        case _: DslBuilder | _: ColBuilder | _: IntPlusMonoid =>
+          dataEnv.getOrElse(te.sym, !!!(s"Cannot resolve companion instance for $te"))
+        case MethodCall(obj, m, args, _) =>
+          val argTypes = getArgTypes(args)
+          val argValues = getArgValues(args)
+          val objValue = dataEnv(obj)
+          val objMethod = getObjMethod(objValue.getClass, m, argTypes)
+          out(objMethod.invoke(objValue, argValues:_*))
+        case ApplyBinOp(op: BinOp[a,r], xSym, ySym) =>
+          val x = dataEnv(xSym)
+          val y = dataEnv(ySym)
+          out(op.applySeq(x, y).asInstanceOf[AnyRef])
+        case Lambda(l, _, x, y) =>
+          val f = (ctx: AnyRef) => {
+            dataEnv += (x -> ctx)
+            l.schedule.foreach(evaluate(_))
+            dataEnv(y)
+          }
+          out(f)
+        case _ => !!!(s"Don't know how to evaluate($te)")
+      }
+    }
+
+    val g = new PGraph(f)
+    g.schedule.foreach(evaluate(_))
     val fun = dataEnv(f).asInstanceOf[SigmaContext => Any]
     val res = (ctx: SigmaContext) => {
       fun(ctx) match {
--- src/test/scala/sigmastate/eval/EvaluationTest.scala
+++ src/test/scala/sigmastate/eval/EvaluationTest.scala
@@ -1,20 +1,37 @@
 package sigmastate.eval
 
 import sigmastate.SInt
-import sigmastate.Values.{IntConstant, LongConstant}
+import sigmastate.Values.{TrueLeaf, LongConstant, FalseLeaf, IntConstant}
 
 import scalan.BaseCtxTests
 import sigmastate.lang.{LangTests, TransformingSigmaBuilder}
 import sigmastate.utxo.CostTable
 import special.sigma.{Box, ContractsTestkit, AnyValue, Context, TestBox, TestContext => TContext}
 
+import scala.collection.mutable
 import scala.util.Success
 
 class EvaluationTest extends BaseCtxTests with LangTests with ContractsTestkit with ExampleContracts {
   lazy val IR = new TestContext with Evaluation {
     import TestSigmaDslBuilder._
+    import ConcreteCostedBuilder._
+    import MonoidBuilderInst._
     val sigmaDslBuilder = RTestSigmaDslBuilder()
     val builder = TransformingSigmaBuilder
+    beginPass(new DefaultPass("mypass",
+      Pass.defaultPassConfig.copy(constantPropagation = false)))
+
+    def getDataEnv: mutable.Map[Sym, AnyRef] = {
+      val monoidBuilder = new special.collection.MonoidBuilderInst()
+      val env = mutable.Map[Sym, AnyRef](
+        sigmaDslBuilder -> new special.sigma.TestSigmaDslBuilder(),
+        sigmaDslBuilder.Cols -> new special.collection.ColOverArrayBuilder,
+        costedBuilder -> new special.collection.ConcreteCostedBuilder(),
+        costedBuilder.monoidBuilder -> monoidBuilder,
+        costedBuilder.monoidBuilder.intPlusMonoid -> monoidBuilder.intPlusMonoid
+      )
+      env
+    }
   }
 
   val noEnv: Map[String, Any] = Map()
@@ -23,6 +40,10 @@ class EvaluationTest extends BaseCtxTests with LangTests with ContractsTestkit w
 
   def newContext(height: Long, self: Box, vars: AnyValue*): TContext = {
     new TContext(noInputs, noOutputs, height, self, vars.toArray)
+  }
+  implicit class TContextOps(ctx: TContext) {
+    def withInputs(inputs: Box*) = new TContext(inputs.toArray, ctx.outputs, ctx.height, ctx.selfBox, ctx.vars)
+    def withOutputs(outputs: Box*) = new TContext(ctx.inputs, outputs.toArray, ctx.height, ctx.selfBox, ctx.vars)
   }
 
   val boxA1 = newAliceBox(1, 100)
@@ -35,10 +56,10 @@ class EvaluationTest extends BaseCtxTests with LangTests with ContractsTestkit w
     emit(name, valueF, costF)
     verifyCostFunc(costF) shouldBe(Success(()))
     verifyIsValid(valueF) shouldBe(Success(()))
-    val costFun = IR.compile[SInt.type](costF)
+    val costFun = IR.compile[SInt.type](getDataEnv, costF)
     val IntConstant(estimatedCost) = costFun(ctx)
     (estimatedCost < CostTable.ScriptLimit) shouldBe true
-    val valueFun = IR.compile(valueF)
+    val valueFun = IR.compile(getDataEnv, valueF)
     valueFun(ctx) shouldBe expected
   }
 
@@ -47,6 +68,29 @@ class EvaluationTest extends BaseCtxTests with LangTests with ContractsTestkit w
 
     check(noEnv, "one", "1", ctx,  IntConstant(1))
     check(noEnv, "oneL", "1L", ctx, LongConstant(1L))
+  }
+
+  test("costed operations") {
+    val ctx = newContext(height = 1, boxA1)
+    check(noEnv, "one+one", "1 + 1", ctx, IntConstant(2))
+    check(noEnv, "oneL+oneL", "1L - 1L", ctx, LongConstant(0))
+    check(noEnv, "one_gt_one", "1 > 1", ctx, FalseLeaf)
+    check(noEnv, "or", "1 > 1 || 2 < 1", ctx, FalseLeaf)
+    check(noEnv, "or2", "1 > 1 || 2 < 1 || 2 > 1", ctx, TrueLeaf)
+    check(noEnv, "or3", "OUTPUTS.size > 1 || OUTPUTS.size < 1", ctx, TrueLeaf)
+    check(noEnv, "and", "1 > 1 && 2 < 1", ctx, FalseLeaf)
+    check(noEnv, "and2", "1 > 1 && 2 < 1 && 2 > 1", ctx, FalseLeaf)
+    check(noEnv, "and3", "OUTPUTS.size > 1 && OUTPUTS.size < 1", ctx, FalseLeaf)
+  }
+
+  test("costed context data") {
+    val ctx = newContext(height = 100, boxA1)
+        .withInputs(boxA1)
+        .withOutputs(boxA2)
+    check(noEnv, "height1", "HEIGHT + 1L", ctx, LongConstant(101))
+    check(noEnv, "height2", "HEIGHT > 1L", ctx, TrueLeaf)
+    check(noEnv, "size", "INPUTS.size + OUTPUTS.size", ctx, IntConstant(2))
+    check(noEnv, "value", "SELF.value + 1L", ctx, LongConstant(101))
   }
 
   test("Crowd Funding") {
