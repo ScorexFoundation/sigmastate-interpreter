fa9c9da4a28cd12a39f1ecbd35a59ab43382ca06
Alex Chepurnoy <alex.chepurnoy@iohk.io>
2018-10-02 11:46:27 +0300

    concrete index in demurrage example

--- src/test/scala/sigmastate/utxo/examples/DemurrageExampleSpecification.scala
+++ src/test/scala/sigmastate/utxo/examples/DemurrageExampleSpecification.scala
@@ -3,6 +3,8 @@ package sigmastate.utxo.examples
 import sigmastate._
 import sigmastate.helpers.{ErgoLikeTestProvingInterpreter, SigmaTestingCommons}
 import org.ergoplatform._
+import sigmastate.Values.ShortConstant
+import sigmastate.interpreter.ContextExtension
 import sigmastate.lang.Terms._
 import sigmastate.utxo.ErgoLikeTestInterpreter
 
@@ -29,10 +31,10 @@ class DemurrageExampleSpecification extends SigmaTestingCommons {
     val demurrageCost = 2
 
     //a blockchain node veryfing a block containing a spending transaction
-    val verifier = new ErgoLikeTestInterpreter
+    val verifier = new ErgoLikeTestInterpreter()
 
     //backer's prover with his private key
-    val userProver = new ErgoLikeTestProvingInterpreter
+    val userProver = new ErgoLikeTestProvingInterpreter().withContextExtender(15, ShortConstant(0))
 
     val regScript = userProver.dlogSecrets.head.publicImage
 
@@ -41,16 +43,18 @@ class DemurrageExampleSpecification extends SigmaTestingCommons {
       "demurrageCost" -> demurrageCost,
       "regScript" -> regScript
     )
+
     val prop = compile(env,
       """{
+        | val outIdx = getVar[Short](15).get
+        | val out = OUTPUTS(outIdx)
+        |
         | val c2 = allOf(Array(
         |   HEIGHT >= SELF.R3[(Long, Array[Byte])].get._1 + demurragePeriod,
-        |   OUTPUTS.exists({ (out: Box) =>
-        |     out.value >= SELF.value - demurrageCost &&
-        |     out.propositionBytes == SELF.propositionBytes &&
-        |     out.R3[(Long, Array[Byte])].get._1 <= HEIGHT &&
+        |     out.value >= SELF.value - demurrageCost ,
+        |     out.propositionBytes == SELF.propositionBytes,
+        |     out.R3[(Long, Array[Byte])].get._1 <= HEIGHT,
         |     out.R3[(Long, Array[Byte])].get._1 >= HEIGHT - 50L
-        |   })
         | ))
         | regScript || c2
         | }
@@ -78,6 +82,8 @@ class DemurrageExampleSpecification extends SigmaTestingCommons {
     val outHeight = 100
     val outValue = 10
 
+    val ce = ContextExtension(Map(15.toByte -> ShortConstant(0)))
+
     //case 1: demurrage time hasn't come yet
     val currentHeight1 = outHeight + demurragePeriod - 1
 
@@ -88,7 +94,8 @@ class DemurrageExampleSpecification extends SigmaTestingCommons {
       lastBlockUtxoRoot = AvlTreeData.dummy,
       boxesToSpend = IndexedSeq(),
       spendingTransaction = tx1,
-      self = createBox(outValue, prop, outHeight))
+      self = createBox(outValue, prop, outHeight),
+      extension = ce)
 
     //user can spend all the money
     val uProof1 = userProver.prove(prop, ctx1, fakeMessage).get.proof
@@ -105,7 +112,8 @@ class DemurrageExampleSpecification extends SigmaTestingCommons {
       lastBlockUtxoRoot = AvlTreeData.dummy,
       boxesToSpend = IndexedSeq(),
       spendingTransaction = tx2,
-      self = createBox(outValue, prop, outHeight))
+      self = createBox(outValue, prop, outHeight),
+      extension = ce)
 
     //user can spend all the money
     val uProof2 = userProver.prove(prop, ctx2, fakeMessage).get.proof
@@ -120,7 +128,8 @@ class DemurrageExampleSpecification extends SigmaTestingCommons {
       lastBlockUtxoRoot = AvlTreeData.dummy,
       boxesToSpend = IndexedSeq(b),
       spendingTransaction = tx3,
-      self = createBox(outValue, prop, outHeight))
+      self = createBox(outValue, prop, outHeight),
+      extension = ce)
 
     assert(ctx3.spendingTransaction.outputs.head.propositionBytes sameElements ctx3.self.propositionBytes)
 
@@ -134,7 +143,8 @@ class DemurrageExampleSpecification extends SigmaTestingCommons {
       lastBlockUtxoRoot = AvlTreeData.dummy,
       boxesToSpend = IndexedSeq(),
       spendingTransaction = tx4,
-      self = createBox(outValue, prop, outHeight))
+      self = createBox(outValue, prop, outHeight),
+      extension = ce)
 
     verifier.verify(prop, ctx4, NoProof, fakeMessage).get._1 shouldBe false
 
@@ -147,7 +157,8 @@ class DemurrageExampleSpecification extends SigmaTestingCommons {
       lastBlockUtxoRoot = AvlTreeData.dummy,
       boxesToSpend = IndexedSeq(),
       spendingTransaction = tx5,
-      self = createBox(outValue, prop, outHeight))
+      self = createBox(outValue, prop, outHeight),
+      extension = ce)
 
     verifier.verify(prop, ctx5, NoProof, fakeMessage).get._1 shouldBe true
   }
