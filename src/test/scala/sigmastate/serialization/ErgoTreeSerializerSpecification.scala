b6f2c85142bac6d8cd82b751076b2aee58c4eacf
Denys Zadorozhnyi <denys@zadorozhnyi.com>
2018-10-26 12:30:35 +0300

    add test with no constants to extract;

--- src/test/scala/sigmastate/serialization/ErgoTreeSerializerSpecification.scala
+++ src/test/scala/sigmastate/serialization/ErgoTreeSerializerSpecification.scala
@@ -1,8 +1,10 @@
 package sigmastate.serialization
 
-import sigmastate.Values.{Constant, ConstantPlaceholder, IntConstant}
+import org.ergoplatform.Self
+import sigmastate.Values.{BlockValue, Constant, ConstantPlaceholder, IntConstant, ValDef, ValUse}
 import sigmastate._
 import sigmastate.helpers.SigmaTestingCommons
+import sigmastate.utxo.{ExtractAmount, ExtractBytes, SizeOf}
 
 class ErgoTreeSerializerSpecification extends SerializationSpecification with SigmaTestingCommons {
 
@@ -12,6 +14,13 @@ class ErgoTreeSerializerSpecification extends SerializationSpecification with Si
     val script = Plus(10, 20)
     val extractedConstants = Seq(IntConstant(10), IntConstant(20))
     val scriptWithPlaceholders = Plus(ConstantPlaceholder(0, SInt), ConstantPlaceholder(1, SInt))
+    ErgoTreeSerializer(IR).extractConstants(script) shouldEqual (extractedConstants, scriptWithPlaceholders)
+  }
+
+  property("no constants to extract") {
+    val script = Plus(ExtractAmount(Self), ExtractAmount(Self))
+    val extractedConstants = Seq()
+    val scriptWithPlaceholders = BlockValue(Vector(ValDef(1,List(),ExtractAmount(Self))),Plus(ValUse(1,SLong),ValUse(1,SLong)))
     ErgoTreeSerializer(IR).extractConstants(script) shouldEqual (extractedConstants, scriptWithPlaceholders)
   }
 
