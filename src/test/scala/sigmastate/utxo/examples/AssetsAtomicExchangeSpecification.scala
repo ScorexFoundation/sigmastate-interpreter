426763985043cede38c936ab8026331abb1f5063
Alex Chepurnoy <alex.chepurnoy@iohk.io>
2018-11-06 10:23:20 +0300

    partial filling example

--- src/test/scala/sigmastate/utxo/examples/AssetsAtomicExchangeSpecification.scala
+++ src/test/scala/sigmastate/utxo/examples/AssetsAtomicExchangeSpecification.scala
@@ -4,9 +4,10 @@ import org.ergoplatform.ErgoBox.R4
 import org.ergoplatform._
 import scorex.crypto.hash.Blake2b256
 import sigmastate.SCollection.SByteArray
-import sigmastate.Values.{ByteArrayConstant, IntConstant, LongConstant, Value}
+import sigmastate.Values.{ByteArrayConstant, IntConstant, LongConstant, ShortConstant, Value}
 import sigmastate._
 import sigmastate.helpers.{ErgoLikeTestProvingInterpreter, SigmaTestingCommons}
+import sigmastate.interpreter.ContextExtension
 import sigmastate.lang.Terms._
 import sigmastate.utxo._
 
@@ -72,8 +73,6 @@ class AssetsAtomicExchangeSpecification extends SigmaTestingCommons {
         EQ(OptionGet(ExtractRegisterAs[SByteArray](ByIndex(Outputs, IntConstant.Zero), R4)), ExtractId(Self))
       )
     )
-
-    //    val x = OR(ConcreteCollection(Vector(AND(ConcreteCollection(Vector(GT(Height, Constant(70, SLong)), ProveDlog(Constant((???, ???, ???, ???), SGroupElement))), SBoolean)), AND(ConcreteCollection(Vector(EQ(SelectField(Apply(ExtractRegisterAs(ByIndex(Outputs, Constant(0, SInt), None), R2, Array[(Array[SByte], SLong)], None), Vector(Constant(0, SInt))), 1), Constant(???, Array[SByte])), GE(SelectField(Apply(ExtractRegisterAs(ByIndex(Outputs, Constant(0, SInt), None), R2, Array[(Array[SByte], SLong)], None), Vector(Constant(0, SInt))), 2), Constant(60, SLong)), EQ(ExtractScriptBytes(ByIndex(Outputs, Constant(0, SInt), None)), Constant(???, Array[SByte])), GE(ExtractAmount(ByIndex(Outputs, Constant(0, SInt), None)), Constant(1, SLong))), SBoolean))), SBoolean))
 
     val buyerEnv = Map("pkA" -> tokenBuyerKey, "deadline" -> deadline, "token1" -> tokenId)
     val altBuyerProp = compile(buyerEnv,
@@ -142,6 +141,151 @@ class AssetsAtomicExchangeSpecification extends SigmaTestingCommons {
       self = input1)
 
     val pr2 = tokenSeller.prove(sellerProp, sellerCtx, fakeMessage).get
+    verifier.verify(sellerProp, sellerCtx, pr2, fakeMessage).get._1 shouldBe true
+
+    println("total cost: " + (buyerProp.cost(buyerCtx) + sellerProp.cost(sellerCtx)))
+  }
+
+  //1 token = 500 nanoErgs
+  property("partial filling") {
+    val tokenBuyer = new ErgoLikeTestProvingInterpreter
+    val tokenSeller = new ErgoLikeTestProvingInterpreter
+    val verifier = new ErgoLikeTestInterpreter
+
+    val tokenId = Blake2b256("token1")
+    val deadline = 70L
+    val tokenBuyerKey = tokenBuyer.dlogSecrets.head.publicImage
+    val tokenSellerKey = tokenBuyer.dlogSecrets.head.publicImage
+
+    val buyerKeyBytes = tokenBuyerKey.bytes
+    val sellerKeyBytes = tokenSellerKey.bytes
+
+    def extractToken(box: Value[SBox.type]) = ByIndex(
+      ExtractRegisterAs(box, ErgoBox.TokensRegId)(ErgoBox.STokensRegType).get, 0)
+
+    def extractTokenId(box: Value[SBox.type]) =
+      SelectField(extractToken(box), 1).asInstanceOf[Value[SByteArray]]
+
+    def extractTokenAmount(box: Value[SBox.type]) =
+      SelectField(extractToken(box), 2).asInstanceOf[Value[SLong.type]]
+
+    val rightProtectionBuyer =
+      EQ(ExtractScriptBytes(ByIndex(Outputs, IntConstant.Zero)), ByteArrayConstant(buyerKeyBytes))
+
+    val rightProtectionSeller =
+      EQ(ExtractScriptBytes(ByIndex(Outputs, IntConstant.One)), ByteArrayConstant(sellerKeyBytes))
+
+    /*
+    val buyerProp = OR(
+      AND(GT(Height, deadline), tokenSellerKey),
+      AND(
+        EQ(extractTokenId(ByIndex(Outputs, IntConstant.Zero)), ByteArrayConstant(tokenId)),
+        GE(extractTokenAmount(ByIndex(Outputs, IntConstant.Zero)), LongConstant(60)),
+        rightProtectionBuyer,
+        EQ(OptionGet(ExtractRegisterAs[SByteArray](ByIndex(Outputs, IntConstant.Zero), R4)), ExtractId(Self))
+      )
+    )*/
+
+    val buyerEnv = Map("pkA" -> tokenBuyerKey, "deadline" -> deadline, "token1" -> tokenId)
+    val buyerProp = compile(buyerEnv,
+      """(HEIGHT > deadline && pkA) || {
+        |
+        |  val outIdx = getVar[Short](127).get
+        |  val out = OUTPUTS(outIdx)
+        |  val tokenData = out.R2[Array[(Array[Byte], Long)]].get(0)
+        |  val tokenId = tokenData._1
+        |  val tokenValue = tokenData._2
+        |  val ergoValue = out.value
+        |  val price = 500
+        |
+        |  allOf(Array(
+        |      tokenId == token1,
+        |      tokenValue >= 1,
+        |      (SELF.value - ergoValue) <= tokenValue * price,
+        |      out.propositionBytes == pkA.propBytes,
+        |      out.R4[Array[Byte]].get == SELF.id
+        |  ))
+        |}
+      """.stripMargin).asBoolValue
+//    altBuyerProp shouldBe buyerProp
+
+      /*
+    val sellerProp = OR(
+      AND(GT(Height, deadline), tokenSellerKey),
+      AND(
+        GE(ExtractAmount(ByIndex(Outputs, IntConstant.One)), LongConstant(100)),
+        EQ(OptionGet(ExtractRegisterAs[SByteArray](ByIndex(Outputs, IntConstant.One), R4)), ExtractId(Self)),
+        rightProtectionSeller
+      )
+    )*/
+
+    val sellerEnv = Map("pkB" -> tokenSellerKey, "deadline" -> deadline)
+    val sellerProp = compile(sellerEnv,
+      """ (HEIGHT > deadline && pkB) || {
+        |   val outIdx = getVar[Short](127).get
+        |   val out = OUTPUTS(outIdx)
+        |
+        |   val tokenData = out.R2[Array[(Array[Byte], Long)]].get(0)
+        |   val tokenId = tokenData._1
+        |   val selfTokenData = SELF.R2[Array[(Array[Byte], Long)]].get(0)
+        |   val selfTokenId = selfTokenData._1
+        |
+        |   val tokenValue = tokenData._2
+        |   val selfTokenValue = selfTokenData._2
+        |
+        |   val selfValue = SELF.value
+        |   val ergoValue = out.value
+        |
+        |   val sold = selfTokenValue - tokenValue
+        |
+        |   val price = 495
+        |
+        |   allOf(Array(
+        |        sold >= 1,
+        |        (ergoValue - selfValue) >= sold*price,
+        |        out.R4[Array[Byte]].get == SELF.id,
+        |        out.propositionBytes == pkB.propBytes
+        |   ))
+        | }
+      """.stripMargin).asBoolValue
+
+  //  sellerProp shouldBe altSellerProp
+
+    //tx inputs
+    val input0 = ErgoBox(10000, buyerProp)
+    val input1 = ErgoBox(1, sellerProp, Seq(tokenId -> 60))
+
+    //tx ouputs
+    val newBox1 = ErgoBox(5000, tokenBuyerKey, Seq(tokenId -> 10), Map(R4 -> ByteArrayConstant(input0.id)))
+    val newBox2 = ErgoBox(5000, tokenSellerKey, Seq(tokenId -> 50), Map(R4 -> ByteArrayConstant(input1.id)))
+    val newBoxes = IndexedSeq(newBox1, newBox2)
+
+    val spendingTransaction = ErgoLikeTransaction(IndexedSeq(), newBoxes)
+
+    val buyerCtx = ErgoLikeContext(
+      currentHeight = 50,
+      lastBlockUtxoRoot = AvlTreeData.dummy,
+      minerPubkey = ErgoLikeContext.dummyPubkey,
+      boxesToSpend = IndexedSeq(input0, input1),
+      spendingTransaction,
+      self = input0,
+      extension = ContextExtension(Map(Byte.MaxValue -> ShortConstant(0))))
+
+    //Though we use separate provers below, both inputs do not contain any secrets, thus
+    //a spending transaction could be created and posted by anyone.
+    val pr = tokenBuyer.withContextExtender(Byte.MaxValue, ShortConstant(0)).prove(buyerProp, buyerCtx, fakeMessage).get
+    verifier.verify(buyerProp, buyerCtx, pr, fakeMessage).get._1 shouldBe true
+
+    val sellerCtx = ErgoLikeContext(
+      currentHeight = 50,
+      lastBlockUtxoRoot = AvlTreeData.dummy,
+      minerPubkey = ErgoLikeContext.dummyPubkey,
+      boxesToSpend = IndexedSeq(input0, input1),
+      spendingTransaction,
+      self = input1,
+      extension = ContextExtension(Map(Byte.MaxValue -> ShortConstant(1))))
+
+    val pr2 = tokenSeller.withContextExtender(Byte.MaxValue, ShortConstant(1)).prove(sellerProp, sellerCtx, fakeMessage).get
     verifier.verify(sellerProp, sellerCtx, pr2, fakeMessage).get._1 shouldBe true
 
     println("total cost: " + (buyerProp.cost(buyerCtx) + sellerProp.cost(sellerCtx)))
