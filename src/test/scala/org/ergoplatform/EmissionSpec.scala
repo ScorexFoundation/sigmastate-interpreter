8d8dfcffb46ee93be5304d54988d4c2fb3655dde
catena <catena@protonmail.com>
2019-01-10 07:49:17 +0300

    No premine box

--- src/test/scala/org/ergoplatform/EmissionSpec.scala
+++ src/test/scala/org/ergoplatform/EmissionSpec.scala
@@ -1,20 +1,48 @@
 package org.ergoplatform
 
 import org.scalacheck.Gen
+import sigmastate.Values.ByteArrayConstant
 import sigmastate.helpers.SigmaTestingCommons
 
 import scala.annotation.tailrec
 
 class EmissionSpec extends SigmaTestingCommons {
 
-  private val GenesisHeight = 1
+  private val EmptyHeight = 0
+  private val GenesisHeight = EmptyHeight + 1
   private val BlocksTotal = 2080799
   private val FixedRatePeriod = 525600
   private val EpochLength = 64800
+  private val MinerRewardDelay = 720
   private val fixedRate = 75L * Emission.CoinsInOneErgo
   private val oneEpochReduction = 3 * Emission.CoinsInOneErgo
   private val foundersInitialReward: Long = fixedRate / 10
   private val coinsTotal = 97739925L * Emission.CoinsInOneErgo
+
+  property("Emission boxes") {
+    val minersRewardTotal = Emission.minersRewardAtHeight(BlocksTotal, FixedRatePeriod, fixedRate, EpochLength, oneEpochReduction, foundersInitialReward)
+    val foundationRewardTotal = Emission.foundationRewardAtHeight(BlocksTotal, FixedRatePeriod, fixedRate, EpochLength, oneEpochReduction, foundersInitialReward)
+    val noPremineProofs = Array("The Guardian Headline", "Last BTC block id", "another source of randomness")
+      .map(_.getBytes("UTF-8"))
+    val boxes = Emission.emissionBoxes(FixedRatePeriod: Int,
+      EpochLength: Int,
+      fixedRate: Long,
+      oneEpochReduction: Long,
+      MinerRewardDelay: Int,
+      EmptyHeight: Int,
+      minersRewardTotal: Long,
+      foundationRewardTotal: Long,
+      noPremineProofs: Seq[Array[Byte]])
+    val emissionBox = boxes.head
+    emissionBox.value shouldBe minersRewardTotal
+    val prop = ErgoScriptPredef.emissionBoxProp(FixedRatePeriod, EpochLength, fixedRate, oneEpochReduction, MinerRewardDelay)
+    emissionBox.proposition shouldBe prop
+
+    val noPremineBox = boxes(1)
+    noPremineProofs.foreach { p =>
+      noPremineBox.additionalRegisters.values.exists(_ == ByteArrayConstant(p)) shouldBe true
+    }
+  }
 
   property("correct sum from miner and foundation parts") {
     // collect coins after the fixed rate period
