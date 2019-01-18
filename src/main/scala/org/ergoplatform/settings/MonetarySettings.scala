9db0bd4e6e965d163e50c816d1a4f8f2b51ac3ec
catena <catena@protonmail.com>
2019-01-18 16:40:25 +0300

    Add fee proposition to monetary settings

--- src/main/scala/org/ergoplatform/settings/MonetarySettings.scala
+++ src/main/scala/org/ergoplatform/settings/MonetarySettings.scala
@@ -1,6 +1,8 @@
 package org.ergoplatform.settings
 
+import org.ergoplatform.ErgoScriptPredef
 import org.ergoplatform.mining.emission.EmissionRules
+import sigmastate.{SBoolean, Values}
 
 /**
   * Configuration file for monetary settings of Ergo chain
@@ -12,4 +14,9 @@ case class MonetarySettings(fixedRatePeriod: Int = 30 * 2 * 24 * 365,
                             fixedRate: Long = 75L * EmissionRules.CoinsInOneErgo,
                             oneEpochReduction: Long = 3L * EmissionRules.CoinsInOneErgo,
                             minerRewardDelay: Int = 720,
-                            foundersInitialReward: Long = 75L * EmissionRules.CoinsInOneErgo / 10)
+                            foundersInitialReward: Long = 75L * EmissionRules.CoinsInOneErgo / 10) {
+
+  val feeProposition: Values.Value[SBoolean.type] = ErgoScriptPredef.feeProposition(minerRewardDelay)
+  val feePropositionBytes: Array[Byte] = feeProposition.bytes
+
+}
