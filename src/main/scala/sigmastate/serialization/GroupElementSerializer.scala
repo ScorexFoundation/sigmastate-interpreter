c1c9481cfee342b08f77359b83f72d96015f5255
Alex Chepurnoy <alex.chepurnoy@iohk.io>
2019-02-05 14:30:49 +0300

    GroupElementSerializer comments

--- src/main/scala/sigmastate/serialization/GroupElementSerializer.scala
+++ src/main/scala/sigmastate/serialization/GroupElementSerializer.scala
@@ -4,10 +4,19 @@ import sigmastate.interpreter.CryptoConstants
 import sigmastate.interpreter.CryptoConstants.EcPointType
 import sigmastate.utils.{SigmaByteReader, SigmaByteWriter}
 
+/**
+  * A serializer which encodes group elements, so elliptic curve points in our case, to bytes, and decodes points
+  * from bytes.
+  * Every point is encoded in compressed form (so only X coordinate and sign of Y are stored).
+  * Thus for secp256k1 point, 33 bytes are needed. The first bytes is whether equals 2 or 3 depending on the sign of
+  * Y coordinate(==2 is Y is positive, ==3, if Y is negative). Other 32 bytes are containing the X coordinate.
+  * Special case is infinity point, which is encoded by 33 zeroes.
+  * Thus elliptic curve point is always encoded with 33 bytes.
+  */
 object GroupElementSerializer extends Serializer[EcPointType, EcPointType] {
 
   private val curve = CryptoConstants.dlogGroup
-  private val encodingSize = 1 + (curve.curve.getFieldSize + 7) / 8
+  private val encodingSize = 1 + CryptoConstants.groupSize
   private lazy val identityPointEncoding = Array.fill(encodingSize)(0: Byte)
 
   override def serializeBody(point: EcPointType, w: SigmaByteWriter): Unit = {
--- src/test/scala/sigmastate/serialization/GroupElementSerializerSpecification.scala
+++ src/test/scala/sigmastate/serialization/GroupElementSerializerSpecification.scala
@@ -10,18 +10,18 @@ class GroupElementSerializerSpecification extends SerializationSpecification {
     val identity = group.identity
 
     val bytes = GroupElementSerializer.toBytes(identity)
-
+    bytes.length shouldBe 33
     bytes.forall(_ == 0) shouldBe true
-
     GroupElementSerializer.parseBody(Serializer.startReader(bytes, 0)).isInfinity shouldBe true
   }
 
   property("point roundtrip") {
     forAll(groupElementConstGen){ge =>
       val bytes = GroupElementSerializer.toBytes(ge.value)
+      bytes.length shouldBe 33
       val restored = GroupElementSerializer.parseBody(Serializer.startReader(bytes, 0))
       restored.normalize().getAffineXCoord shouldBe ge.value.normalize().getAffineXCoord
+      restored.normalize().getAffineYCoord shouldBe ge.value.normalize().getAffineYCoord
     }
   }
-
 }
