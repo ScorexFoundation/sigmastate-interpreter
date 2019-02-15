5aede1cbe640f44214ea271cbae811718f9e48d7
Denys Zadorozhnyi <denys@zadorozhnyi.com>
2019-02-15 15:51:05 +0200

    add sourceContext info in the SigmaException message;

--- src/main/scala/sigmastate/lang/exceptions/Exceptions.scala
+++ src/main/scala/sigmastate/lang/exceptions/Exceptions.scala
@@ -3,7 +3,14 @@ package sigmastate.lang.exceptions
 import sigmastate.lang.SourceContext
 
 class SigmaException(val message: String, val source: Option[SourceContext] = None)
-    extends Exception(message)
+    extends Exception(message) {
+
+  override def getMessage: String = source.map { srcCtx =>
+    val lineNumberStrPrefix = s"line ${srcCtx.line}: "
+    "\n" + lineNumberStrPrefix +
+      s"${srcCtx.sourceLine}\n${" " * (lineNumberStrPrefix.length + srcCtx.column - 1)}^\n" + message
+  }.getOrElse(message)
+}
 
 class BinderException(message: String, source: Option[SourceContext] = None)
     extends SigmaException(message, source)
