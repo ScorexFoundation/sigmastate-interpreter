83c2d68ba8499fae888f6ed0aa89562720f51a34
scalahub <scalahub@gmail.com>
2019-02-23 23:01:35 +0530

    added advanced cold-wallet contract example

--- src/test/scala/sigmastate/utxo/examples/ColdWalletAdvContractExampleSpecification.scala
+++ src/test/scala/sigmastate/utxo/examples/ColdWalletAdvContractExampleSpecification.scala
@@ -1,6 +1,6 @@
 package sigmastate.utxo.examples
 
-import org.ergoplatform.ErgoBox.{R4, R5}
+import org.ergoplatform.ErgoBox.{R4, R5, R6}
 import org.ergoplatform._
 import sigmastate.AvlTreeData
 import sigmastate.Values.{IntConstant, LongConstant}
@@ -28,7 +28,8 @@ class ColdWalletAdvContractExampleSpecification extends SigmaTestingCommons {
     val carolPubKey = carol.dlogSecrets.head.publicImage
 
     val blocksIn24h = 500
-    val percent = 1
+    val percent1Key = 1
+    val percent2Key = 10
     val minSpend = 100
 
     val env = Map(
@@ -37,37 +38,59 @@ class ColdWalletAdvContractExampleSpecification extends SigmaTestingCommons {
       "user2" -> bobPubKey,
       "user3" -> carolPubKey,
       "blocksIn24h" -> IntConstant(blocksIn24h),
-      "percent" -> IntConstant(percent),
+      "percent1Key" -> IntConstant(percent1Key),
+      "percent2Key" -> IntConstant(percent2Key),
       "minSpend" -> IntConstant(minSpend)
     )
 
+    // assume that person topping up this address creates the box correctly... i.e., sets the correct values of R4 and R5
+    // To Do: example that allows spending multiple Boxes with the same script
+    //        idea is as follows:
+    //           1. make global avbl1Key as sum of all avbl1Key's, where each avbl1Key is computed via its own start
+    //           2. make global avbl2Key as sum of all avbl2Key's, where each avbl2Key is computed via its own start
+    //           3. make start of output box as max of all starts of the inputs
+    //           4. output must contain at least one box of same type with min bal based on avbl1Key/avbl2Key
+
     val script = compileWithCosting(env,
       """{
-        |  val min = SELF.R5[Long].get // min balance needed in this period
         |  val depth = HEIGHT - SELF.creationInfo._1 // number of confirmations
         |  val start = min(depth, SELF.R4[Int].get) // height at which period started
+        |
         |  val notExpired = HEIGHT - start <= blocksIn24h
-        |
-        |  val ours:Long = SELF.value - SELF.value * percent / 100
-        |  val keep = if (ours > minSpend) ours else 0L
-        |
         |  val newStart:Int = if (notExpired) start else HEIGHT
-        |  val newMin:Long = if (notExpired) min else keep
         |
-        |  val isValid = {(someMin:Long) =>
-        |   OUTPUTS.exists({(out:Box) =>
+        |  // available for one user to spend in this period
+        |  val avbl1Key = if (notExpired)
+        |                  SELF.R5[Long].get
+        |                else
+        |                  max(SELF.value * percent1Key / 100, minSpend)
+        |
+        |  // available for two users to spend in this period
+        |  val avbl2Key = if (notExpired)
+        |                  SELF.R6[Long].get
+        |                else
+        |                  max(SELF.value * percent2Key / 100, minSpend)
+        |
+        |  // to do: double check if negatives values of avbl1Key create any problem
+        |  val isValid1Key = INPUTS.size == 1 && OUTPUTS.exists({(out:Box) =>
         |    out.propositionBytes == SELF.propositionBytes &&
-        |    out.value >= someMin &&
         |    out.R4[Int].get >= newStart &&
-        |    out.R5[Long].get == someMin
-        |   })
-        |  }
+        |    out.value >= SELF.value - avbl1Key &&
+        |    out.value - out.R5[Long].get == SELF.value - avbl1Key &&
+        |    out.R6[Long].get == SELF.R6[Long].get
+        |  })
+        |
+        |  val isValid2Key = INPUTS.size == 1 && OUTPUTS.exists({(out:Box) =>
+        |    out.propositionBytes == SELF.propositionBytes &&
+        |    out.R4[Int].get >= newStart &&
+        |    out.value >= SELF.value - avbl2Key &&
+        |    out.value - out.R6[Long].get == SELF.value - avbl2Key &&
+        |    out.R5[Long].get == SELF.R5[Long].get
+        |  })
         |
         |  allOf(Coll(user1, user2, user3)) || (
-        |    min >= keep && // topup should keep min > keep to ensure spendable by anyOf(user1, user2, user3)
-        |    anyOf(Coll(user1, user2, user3)) &&
-        |    (newMin == 0 || isValid(newMin)) // error here
-        |    //(newMin == 0 || isValid(9900L)) // works
+        |    (anyOf(Coll(user1, user2, user3)) && (SELF.value <= avbl1Key || isValid1Key)) ||
+        |    (atLeast(2, Coll(user1, user2, user3)) && (SELF.value <= avbl2Key || isValid2Key))
         |  )
         |}""".stripMargin).asBoolValue
 
@@ -75,14 +98,16 @@ class ColdWalletAdvContractExampleSpecification extends SigmaTestingCommons {
 
     // someone creates a transaction that outputs a box depositing money into the wallet.
     // In the example, we don't create the transaction; we just create a box below
-    val depositAmount = 10000L
+    val depositAmount = 100000L
     val depositHeight = 100
-    val min = depositAmount - depositAmount * percent/100
+    val avbl1Key = depositAmount * percent1Key/100
+    val avbl2Key = depositAmount * percent2Key/100
 
     val depositOutput = ErgoBox(depositAmount, address.script, depositHeight, Nil,
       Map(
-        R4 -> IntConstant(depositHeight), // can keep any value in R4 initially
-        R5 -> LongConstant(min) // keeping it below min will make UTXO unspendable
+        R4 -> IntConstant(depositHeight), // can keep value in R4 initially
+        R5 -> LongConstant(avbl1Key), // keeping it below min will make UTXO unspendable
+        R6 -> LongConstant(avbl2Key) // keeping it below min will make UTXO unspendable
       )
     )
 
@@ -94,39 +119,68 @@ class ColdWalletAdvContractExampleSpecification extends SigmaTestingCommons {
     val spendEnv = Map(ScriptNameProp -> "spendEnv")
 
     // One of Alice, Bob  or Carol withdraws
-    val firstWithdrawAmount = depositAmount * percent / 100 // less than or eqaul to percent
-    val firstChangeAmount = depositAmount - firstWithdrawAmount
+    val firstWithdrawAmount1Key = depositAmount * percent1Key / 100 // less than or equal to percent
+    val firstChangeAmount1Key = depositAmount - firstWithdrawAmount1Key
 
-    val firstChangeOutput = ErgoBox(firstChangeAmount, address.script, firstWithdrawHeight, Nil,
+    val firstChangeOutput1Key = ErgoBox(firstChangeAmount1Key, address.script, firstWithdrawHeight, Nil,
       Map(
         R4 -> IntConstant(depositHeight), // newStart (= old start)
-        R5 -> LongConstant(min) // newMin (= old min)
+        R5 -> LongConstant(avbl1Key - firstWithdrawAmount1Key), // new avbl1Key (= 0)
+        R6 -> LongConstant(avbl2Key) // new avbl2Key (= old avbl2Key)
       )
     )
-    val firstWithdrawOutput = ErgoBox(firstWithdrawAmount, carolPubKey, firstWithdrawHeight)
+    val firstWithdrawOutput1Key = ErgoBox(firstWithdrawAmount1Key, carolPubKey, firstWithdrawHeight)
 
     //normally this transaction would be invalid, but we're not checking it in this test
-    val firstWithdrawTx = ErgoLikeTransaction(IndexedSeq(), IndexedSeq(firstChangeOutput, firstWithdrawOutput))
+    val firstWithdrawTx1Key = ErgoLikeTransaction(IndexedSeq(), IndexedSeq(firstChangeOutput1Key, firstWithdrawOutput1Key))
 
-    val firstWithdrawContext = ErgoLikeContext(
+    val firstWithdrawContext1Key = ErgoLikeContext(
       currentHeight = firstWithdrawHeight,
       lastBlockUtxoRoot = AvlTreeData.dummy,
       minerPubkey = ErgoLikeContext.dummyPubkey,
       boxesToSpend = IndexedSeq(depositOutput),
-      spendingTransaction = firstWithdrawTx,
+      spendingTransaction = firstWithdrawTx1Key,
       self = depositOutput
     )
 
     val verifier = new ErgoLikeTestInterpreter
 
-    val proofAliceWithdraw = alice.prove(spendEnv, script, firstWithdrawContext, fakeMessage).get.proof
-    verifier.verify(env, script, firstWithdrawContext, proofAliceWithdraw, fakeMessage).get._1 shouldBe true
+    val proofAliceWithdraw = alice.prove(spendEnv, script, firstWithdrawContext1Key, fakeMessage).get.proof
+    verifier.verify(env, script, firstWithdrawContext1Key, proofAliceWithdraw, fakeMessage).get._1 shouldBe true
 
-    val proofBobWithdraw = bob.prove(env, script, firstWithdrawContext, fakeMessage).get.proof
-    verifier.verify(env, script, firstWithdrawContext, proofBobWithdraw, fakeMessage).get._1 shouldBe true
+    val proofBobWithdraw = bob.prove(env, script, firstWithdrawContext1Key, fakeMessage).get.proof
+    verifier.verify(env, script, firstWithdrawContext1Key, proofBobWithdraw, fakeMessage).get._1 shouldBe true
 
-    val proofCarolWithdraw = carol.prove(env, script, firstWithdrawContext, fakeMessage).get.proof
-    verifier.verify(env, script, firstWithdrawContext, proofCarolWithdraw, fakeMessage).get._1 shouldBe true
+    val proofCarolWithdraw = carol.prove(env, script, firstWithdrawContext1Key, fakeMessage).get.proof
+    verifier.verify(env, script, firstWithdrawContext1Key, proofCarolWithdraw, fakeMessage).get._1 shouldBe true
+
+    // any two of Alice, Bob  or Carol withdraws
+    val firstWithdrawAmount2Key = depositAmount * percent2Key / 100 // less than or equal to percent
+    val firstChangeAmount2Key = depositAmount - firstWithdrawAmount2Key
+
+    val firstChangeOutput2Key = ErgoBox(firstChangeAmount2Key, address.script, firstWithdrawHeight, Nil,
+      Map(
+        R4 -> IntConstant(depositHeight), // newStart (= old start)
+        R5 -> LongConstant(avbl1Key), // new avbl1Key (= 0)
+        R6 -> LongConstant(avbl2Key - firstWithdrawAmount2Key) // new avbl2Key (= old avbl2Key)
+      )
+    )
+    val firstWithdrawOutput2Key = ErgoBox(firstWithdrawAmount2Key, carolPubKey, firstWithdrawHeight)
+
+    //normally this transaction would be invalid, but we're not checking it in this test
+    val firstWithdrawTx2Key = ErgoLikeTransaction(IndexedSeq(), IndexedSeq(firstChangeOutput2Key, firstWithdrawOutput2Key))
+
+    val firstWithdrawContext2Key = ErgoLikeContext(
+      currentHeight = firstWithdrawHeight,
+      lastBlockUtxoRoot = AvlTreeData.dummy,
+      minerPubkey = ErgoLikeContext.dummyPubkey,
+      boxesToSpend = IndexedSeq(depositOutput),
+      spendingTransaction = firstWithdrawTx2Key,
+      self = depositOutput
+    )
+
+    val proofAliceBobWithdraw = alice.withSecrets(bob.dlogSecrets).prove(spendEnv, script, firstWithdrawContext2Key, fakeMessage).get.proof
+    verifier.verify(env, script, firstWithdrawContext2Key, proofAliceBobWithdraw, fakeMessage).get._1 shouldBe true
 
   }
 }
