cd81e36dcd00838d98d682a8fe16b8d584123825
Alex Chepurnoy <alex.chepurnoy@iohk.io>
2019-04-08 10:35:17 +0300

    comments, zeroUserBalance check

--- src/test/scala/sigmastate/utxo/examples/LetsSpecification.scala
+++ src/test/scala/sigmastate/utxo/examples/LetsSpecification.scala
@@ -71,34 +71,38 @@ class LetsSpecification extends SigmaTestingCommons { suite =>
     """{
       |
       | val selfOut = OUTPUTS(0)
+      |
+      | // Checks that the management label token is replicating self
       | val outTokenCorrect = (selfOut.tokens.size == 1) && (selfOut.tokens(0)._1 == letsToken)
       |
-      | val selfTree = SELF.R4[AvlTree]
-      |
+      | //The management script is replicating self
       | val scriptCorrect = selfOut.propositionBytes == SELF.propositionBytes
       |
-      |
+      | // A spending transaction is creating boxes for directory, user, fee.
       | val outsSizeCorrect = OUTPUTS.size == 3
       |
+      | // Checks that new token is issued, and its amount is correct
+      | // OUTPUTS(0) tokens already checked
       | val issuedTokenId = INPUTS(0).id
-      |
       | val userOut = OUTPUTS(1)
-      |
       | val correctTokenAmounts =
       |   (userOut.tokens.size == 1 &&
       |     userOut.tokens(0)._1 == issuedTokenId &&
       |     userOut.tokens(0)._2 == 1 &&
       |     OUTPUTS(2).tokens.size == 0)
       |
+      |  // Checks that the new user has been created with the zero balance
+      |  val zeroUserBalance  = userOut.R4[Long].get == 0
+      |
+      |  // Checks that the new token identifier has been added to the directory
+      |  val selfTree = SELF.R4[AvlTree].get
       |  val toAdd: Coll[(Coll[Byte], Coll[Byte])] = Coll((issuedTokenId, Coll[Byte]()))
-      |
       |  val proof = getVar[Coll[Byte]](1).get
-      |  val modifiedTree = SELF.R5[AvlTree].get.insert(toAdd, proof).get
+      |  val modifiedTree = selfTree.insert(toAdd, proof).get
       |  val expectedTree = selfOut.R5[AvlTree].get
+      |  val treeCorrect = modifiedTree == expectedTree
       |
-      | val treeCorrect = modifiedTree == expectedTree
-      |
-      | correctTokenAmounts && outTokenCorrect && scriptCorrect && treeCorrect
+      | correctTokenAmounts && outTokenCorrect && scriptCorrect && treeCorrect && zeroUserBalance
       |}""".stripMargin
   )
 
