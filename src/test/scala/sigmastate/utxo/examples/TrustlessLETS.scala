01fd86bec5994474b60b9c44740c02f170c06e98
scalahub <scalahub@gmail.com>
2019-05-21 23:04:10 +0530

    Enhanced Trustless LETS example

--- src/test/scala/sigmastate/utxo/examples/TrustlessLETS.scala
+++ src/test/scala/sigmastate/utxo/examples/TrustlessLETS.scala
@@ -14,41 +14,54 @@ class TrustlessLETS extends SigmaTestingCommons {
     val rateTokenID = Blake2b256("rate")
     val letsTokenID = Blake2b256("lets")
     val minErgsToJoin = 100L // to prevent spam and DDoS attacks
+    val minWithdrawTime = 1000 // Ergs locked till 1000 blocks
     val memberBoxEnv = Map(
       ScriptNameProp -> "memberBoxScriptEnv",
       "rateTokenID" -> rateTokenID,
-      "letsTokenID" -> letsTokenID
+      "letsTokenID" -> letsTokenID,
+      "minWithdrawTime" -> minWithdrawTime
     )
 
     val memberBoxScript = compile(memberBoxEnv,
       """{
-        |val validRate = CONTEXT.dataInputs(0).tokens(0)._1 == rateTokenID
+        |val validRateOracle = CONTEXT.dataInputs(0).tokens(0)._1 == rateTokenID
         |val rate = CONTEXT.dataInputs(0).R4[Int].get
         |val inBalance = SELF.R4[Long].get
         |val pubKey = SELF.R5[SigmaProp].get
+        |val createdAt = SELF.R6[Long].get
+        |
         |val index = getVar[Int](0).get
         |val outBalance = OUTPUTS(index).R4[Long].get
-        |val letsInputs = INPUTS.filter({(b:Box) => b.propositionBytes == SELF.propositionBytes})
-        |val letsOutputs = OUTPUTS.filter({(b:Box) => b.propositionBytes == SELF.propositionBytes})
+        |
+        |val isMemberBox = {(b:Box) => b.propositionBytes == SELF.propositionBytes}
+        |val letsInputs = INPUTS.filter(isMemberBox)
+        |val letsOutputs = OUTPUTS.filter(isMemberBox)
         |
         |// receiver box will contain same amount of ergs.
         |val receiver = outBalance > inBalance && OUTPUTS(index).value == SELF.value
-        |val totalInBalance = letsInputs.map({(b:Box) => b.R4[Long].get}).fold(0L, {(l:Long, r:Long) => l + r})
-        |val totalOutBalance = letsOutputs.map({(b:Box) => b.R4[Long].get}).fold(0L, {(l:Long, r:Long) => l + r})
+        |val letsBalance = {(b:Box) => b.R4[Long].get}
+        |
+        |val letsIn = letsInputs.map(letsBalance).fold(0L, {(l:Long, r:Long) => l + r})
+        |val letsOut = letsOutputs.map(letsBalance).fold(0L, {(l:Long, r:Long) => l + r})
         |
         |// sender box can contain less amount of ergs (sender may withdraw ergs provided that any
         |// negative LETS balance of sender is backed by sufficient ergs
         |// for receiver, we don't touch the erg balance, since a receiver is not actively involved
         |// in the transaction
         |
-        |val correctErgs = outBalance > 0 || OUTPUTS(index).value >= -outBalance * rate
+        |val correctErgs = OUTPUTS(index).value >= -outBalance * rate && (
+        |  OUTPUTS(index).value >= SELF.value || SELF.R6[Long].get + minWithdrawTime > HEIGHT
+        |)
         |
+        |inBalance != outBalance && // some transaction should occur
         |SELF.tokens(0)._1 == letsTokenID &&
         |OUTPUTS(index).tokens(0)._1 == letsTokenID &&
-        |validRate && totalInBalance == totalOutBalance &&
+        |validRateOracle &&
+        |letsIn == letsOut &&
         |letsInputs.size == 2 && letsOutputs.size == 2 &&
         |OUTPUTS(index).propositionBytes == SELF.propositionBytes &&
         |OUTPUTS(index).R5[SigmaProp].get == pubKey &&
+        |OUTPUTS(index).R6[Long].get == SELF.R6[Long].get && // creation height
         |(receiver || (pubKey && correctErgs))
         |}""".stripMargin
     ).asSigmaProp
@@ -69,7 +82,8 @@ class TrustlessLETS extends SigmaTestingCommons {
         |   b.tokens(0)._1 == letsTokenID && b.tokens(0)._2 == 1 &&
         |   blake2b256(b.propositionBytes) == memberBoxScriptHash &&
         |   SELF.R4[Long].get == 0 && // start box with zero LETS balance
-        |   b.value >= minErgsToJoin
+        |   b.value >= minErgsToJoin &&
+        |	  b.R6[Long].get <= HEIGHT // store the creation height in R6
         |}
         |val numLetsBoxes = OUTPUTS.filter({(b:Box) => isLets(b)}).size
         |tokenBox.tokens(0)._1 == SELF.tokens(0)._1 &&
