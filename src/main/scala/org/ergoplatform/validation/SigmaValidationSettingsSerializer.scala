02d3c1620c3e9d1ec941088197685bad762c7cd9
Alexander Slesarenko <avslesarenko@gmail.com>
2019-05-30 17:13:37 +0300

    serialize ruleId as offsets

--- src/main/scala/org/ergoplatform/validation/SigmaValidationSettingsSerializer.scala
+++ src/main/scala/org/ergoplatform/validation/SigmaValidationSettingsSerializer.scala
@@ -8,12 +8,14 @@ import sigma.util.Extensions.{IntOps,LongOps}
   * This serializer preserves roundtrip identity `deserialize(serialize(_)) = identity`
   * however it may not preserve `serialize(deserialize(_)) = identity` */
 object SigmaValidationSettingsSerializer extends SigmaSerializer[SigmaValidationSettings, SigmaValidationSettings] {
+  import ValidationRules._
 
   override def serialize(settings: SigmaValidationSettings, w: SigmaByteWriter): Unit = {
     val rules = settings.toArray.sortBy(_._1)
     w.putUInt(rules.length)
     rules.foreach { r =>
-      w.putUShort(r._1)
+      val ofs = (r._1 - FirstRuleId).toShort
+      w.putUShort(ofs)
       RuleStatusSerializer.serialize(r._2._2, w)
     }
   }
@@ -21,7 +23,7 @@ object SigmaValidationSettingsSerializer extends SigmaSerializer[SigmaValidation
   override def parse(r: SigmaByteReader): SigmaValidationSettings = {
     val nRules = r.getUInt().toIntExact
     val parsed = (0 until nRules).map { _ =>
-      val ruleId = r.getUShort().toShortExact
+      val ruleId = (r.getUShort() + FirstRuleId).toShortExact
       val status = RuleStatusSerializer.parse(r)
       ruleId -> status
     }
