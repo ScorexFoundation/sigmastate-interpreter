016d8153ad39ca728bdc7ae9f265de89312b3e56
Alexander Slesarenko <avslesarenko@gmail.com>
2019-06-17 10:43:04 +0300

    GenSerializers  description

--- src/test/scala/sigmastate/serialization/PDHTSerializerSpecification.scala
+++ src/test/scala/sigmastate/serialization/PDHTSerializerSpecification.scala
@@ -10,11 +10,18 @@ class PDHTSerializerSpecification extends SerializationSpecification {
     forAll { i: ProveDHTuple =>
       roundTripTest(i.toSigmaProp)
     }
+
     // In IntelliJ IDEA this test is executed last, at this point all statistics has been collected
-    // We output it here in the console
-//    println(CostTableStat.costTableString)
-//    println(ValueSerializer.printSerInfo())
-//    GenSerializers.generateSerSpec()
+    // uncomment to output it in the console
+    // println(CostTableStat.costTableString)
+
+    // uncomment to output info about serializers
+    // println(ValueSerializer.printSerInfo())
+
+    // ValueSerializer.serializerInfo is ready to be printed after all serializers are executed
+    // at least once during test execution.
+    // uncomment to save serialization info to the file
+    // GenSerializers.generateSerSpec()
   }
 
 }
