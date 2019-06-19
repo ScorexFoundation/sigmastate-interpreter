df34f2fa2b689c9a6644f51eb7dfc898257a6f64
Alex Chepurnoy <alex.chepurnoy@iohk.io>
2019-06-19 17:13:06 +0300

    comments to Hint family

--- src/main/scala/sigmastate/interpreter/Hint.scala
+++ src/main/scala/sigmastate/interpreter/Hint.scala
@@ -14,23 +14,42 @@ import sigmastate.basics.FirstProverMessage
 trait Hint
 
 /**
-  *
+  * A hint which is indicating that a secret associated with its public image "image" is known by some other party.
   */
 trait OtherSecret extends Hint {
   val image: SigmaBoolean
 }
 
+/**
+  * A hint which is contains a proof-of-knowledge for a secret associated with its public image "image".
+  */
+case class OtherSecretProven(override val image: SigmaBoolean, uncheckedTree: UncheckedTree) extends OtherSecret
+
+/**
+  * A family of hints which are about a correspondence between a public image of a secret image and prover's commitment
+  * to randomness ("a" in a sigma protocol).
+  */
 trait CommitmentHint extends Hint {
   val image: SigmaBoolean
   val commitment: FirstProverMessage
 }
 
+/**
+  * A hint which a commitment to randomness associated with a public image of a secret, as well as randomness itself.
+  * Please note that this randomness should be kept in secret by the prover.
+  *
+  * @param image - image of a secret
+  * @param randomness - randomness
+  * @param commitment - commitment to randomness used while proving knowledge of the secret
+  */
 case class OwnCommitment(override val image: SigmaBoolean, randomness: BigInteger, commitment: FirstProverMessage) extends CommitmentHint
 
+/**
+  * A hint which contains a commitment to randomness associated with a public image of a secret.
+  * @param image - image of a secret
+  * @param commitment - commitment to randomness used while proving knowledge of the secret
+  */
 case class OtherCommitment(override val image: SigmaBoolean, commitment: FirstProverMessage) extends OtherSecret with CommitmentHint
-
-case class OtherSecretProven(override val image: SigmaBoolean, uncheckedTree: UncheckedTree) extends OtherSecret
-
 
 /**
   * Collection of hints to be used by a prover
