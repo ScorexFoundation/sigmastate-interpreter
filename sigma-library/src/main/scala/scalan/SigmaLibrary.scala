79cd5c7379c070447e0c1aeb6a127d4ab754f8c8
Alexander Slesarenko <avslesarenko@gmail.com>
2019-07-29 09:53:00 +0300

    removed WSigmaPredef

--- sigma-library/src/main/scala/scalan/SigmaLibrary.scala
+++ sigma-library/src/main/scala/scalan/SigmaLibrary.scala
@@ -1,15 +1,11 @@
 package scalan
 
-import org.bouncycastle.crypto.ec.CustomNamedCurves
-import org.bouncycastle.math.ec.ECPoint
 import special.sigma._
-import special.sigma.wrappers.WrappersSpecModule
 
 import scala.collection.mutable.ArrayBuffer
 
 trait SigmaLibrary extends Library
     with special.sigma.wrappers.WrappersModule
-    with WrappersSpecModule
     with SigmaDslModule
     with CostedObjectsModule
     with SigmaDslCostedModule
--- sigma-library/src/main/scala/special/sigma/wrappers/WrappersModule.scala
+++ sigma-library/src/main/scala/special/sigma/wrappers/WrappersModule.scala
@@ -1,7 +1,4 @@
 package special.sigma.wrappers
 
-import wrappers.special.sigma.WSigmaPredefsModule
-
 trait WrappersModule
   extends special.wrappers.WrappersModule
-     with WSigmaPredefsModule
