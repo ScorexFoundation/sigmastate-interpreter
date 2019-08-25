e6c961b5d07f70e47c814b635e62e93a6ecb26d1
Eugene Gostkin <gostkin.em@phystech.edu>
2019-08-25 18:07:10 +0300

    Implement JsonSerialization for primitives, colls, pairs

--- src/main/scala/sigmastate/serialization/DataJsonEncoder.scala
+++ src/main/scala/sigmastate/serialization/DataJsonEncoder.scala
@@ -1,21 +1,26 @@
 package sigmastate.serialization
 
+import java.math.BigInteger
 import java.nio.charset.StandardCharsets
 
 import io.circe._
 import io.circe.syntax._
+import scalan.RType
 import sigmastate._
-import sigmastate.eval.SigmaDsl
+import sigmastate.eval.{Colls, Evaluation, SigmaDsl}
+import sigmastate.lang.SigmaParser
 import sigmastate.lang.exceptions.SerializerException
-import special.collection.Coll
+import special.collection.{Coll, collRType}
 import special.sigma.BigInt
 import spire.syntax.all.cfor
 
 import scala.collection.mutable
-import scala.collection.mutable.{MutableList, Stack}
 
-object STypeNameEncoder {
-  def encode[T <: SType](tpe: T): String = tpe match {
+object DataJsonEncoder {
+  implicit private val encodeBytes: Encoder[Array[Byte]] = (bytes: Array[Byte]) =>
+    Json.fromString(bytes.toString)
+
+  private def encodeName[T <: SType](tpe: T): String = tpe match {
     case SUnit => "Unit"
     case SBoolean => "Boolean"
     case SByte => "Byte"
@@ -25,115 +30,22 @@ object STypeNameEncoder {
     case SBigInt => "BigInt"
     case SString => "String"
     case tColl: SCollectionType[a] =>
-      "Coll[" + encode(tColl.elemType) + "]"
+      "Coll[" + encodeName(tColl.elemType) + "]"
+    case t: STuple =>
+      "(" + t.items.map((x: SType) => s"${encodeName(x)}").mkString(",") + ")"
     case t => throw new SerializerException(s"Not defined DataJsonEncoder for ${t}")
   }
 
-  sealed trait Chunk
-
-  case class NameChunk(data: String)
-  case class ParenthesesChunk(chunks: List[Chunk])
-  case class BracketsChunk(typename: NameChunk, wrappedTypeChunk: Chunk)
-
-  private class ChunkParser(from: String) {
-    var position = 0
-
-    private def checkPosition(pos: Int): Unit = {
-      if (pos >= from.length) {
-        throw new SerializerException("Out of bounds character search.")
-      }
-    }
-    private def getBracketChunk(typename: NameChunk): BracketsChunk = {
-      val wrappedChunks = getChunks
-      val newPosition = position + 1
-      checkPosition(newPosition)
-      if (from(newPosition) != ']') {
-        throw new SerializerException("Bad brackets balance: closing `]` is not found at right level")
-      }
-      position += 1
-      BracketsChunk(typename, wrappedChunks)
-    }
-
-    private def getParenthesesChunk: ParenthesesChunk = {
-      var chunks = mutable.MutableList.empty[Chunk]
-      chunks += getChunks
-      position += 1
-      checkPosition(position)
-      while (from(position) == ',') {
-        chunks += getChunks
-        val newPosition = position + 1
-        checkPosition(newPosition)
-      }
-      if (from(position) != ')') {
-        throw new SerializerException("Bad brackets balance: closing `)` is not found at right level")
-      }
-      ParenthesesChunk(chunks.toList)
-    }
-
-    private def getNameChunk: NameChunk = {
-      var name: String = ""
-      while (from(position).isLetter && 'A'.t)
-    }
-    def getChunks: Chunk = {
-      checkPosition(position)
-
-      from(position) match {
-        case  =>
-          position += 1
-          getBracketChunk()
-      }
-    }
-  }
-  private class Decoder(from: String) {
-    private var stack = mutable.MutableList.empty[Chunk]
-    var position = 0
-
-    private def isUsualLetter(c: Char): Boolean = c.isLetter && c >= 'A' && c <= 'z'
-
-    private def readTypeName(): String = {
-      var result = ""
-      while (isUsualLetter(from(position))) {
-        result += from(position)
-      }
-      result
-    }
-
-    private def chunkParse: Chunk = {
-      if (position >= from.length) {
-        throw new SerializerException(s"Can't shrink to chunks ${from}.")
-      }
-      from(position) match {
-        case '(' =>
-          var parenthesisLevel = 0
-          var currentPosition = position + 1
-          while (currentPosition < from.length) {
-            match from(curr)
-            currentPosition += 1
-          }
-          stack += new Chunk(ChunkType.OpeningBracket)
-
-      }
-
-      return SUnit
-    }
-    def get: SType = {
-
-    }
-  }
-  def decode(from: String): SType = {
-
+  def encode[T <: SType](v: T#WrappedType, tpe: T): Json = {
+    val encodedType = encodeName(tpe)
+    val encodedData = encodeData(v, tpe)
+    Json.obj(
+      "type" -> Json.fromString(encodedType),
+      "value" -> encodedData,
+    )
   }
 
-
-
-}
-object DataJsonEncoder {
-  implicit private val encodeBytes: Encoder[Array[Byte]] = (bytes: Array[Byte]) => Json.obj(
-    "length" -> Json.fromInt(bytes.length),
-    "data" -> Json.fromString(bytes.toString)
-  )
-
-  private def encodeSTypeData[T <: SType](v: T#WrappedType, tpe: T): Json = tpe match {
+  private def encodeData[T <: SType](v: T#WrappedType, tpe: T): Json = tpe match {
     case SUnit => v.asInstanceOf[Unit].asJson
     case SBoolean => v.asInstanceOf[Boolean].asJson
     case SByte => v.asInstanceOf[Byte].asJson
@@ -141,77 +53,94 @@ object DataJsonEncoder {
     case SInt => v.asInstanceOf[Int].asJson
     case SLong => v.asInstanceOf[Long].asJson
     case SBigInt =>
-      val bytes = SigmaDsl.toBigInteger(v.asInstanceOf[BigInt]).toByteArray
-      bytes.asJson
+      java.util.Base64.getEncoder.encodeToString(v.asInstanceOf[BigInt].toBytes.toArray).asJson
     case SString =>
-      val bytes = v.asInstanceOf[String].getBytes(StandardCharsets.UTF_8)
-      bytes.asJson
+      java.util.Base64.getEncoder.encodeToString(v.asInstanceOf[String].getBytes).asJson
     case tColl: SCollectionType[a] =>
       val coll = v.asInstanceOf[tColl.WrappedType]
-      tColl.elemType match {
-        case SBoolean =>
-          coll.asInstanceOf[Coll[Boolean]].toArray.asJson
-        case SByte =>
-          coll.asInstanceOf[Coll[Byte]].toArray.asJson
-        case _ =>
-          val arr = coll.toArray
-          val jsons = mutable.MutableList.empty[Json]
-          cfor(0)(_ < arr.length - 1, _ + 1) { i =>
-            val x = arr(i)
-            jsons += encodeSTypeData(x, tColl.elemType)
-          }
-          jsons.asJson
+      val arr = coll.toArray
+      var jsons = mutable.MutableList.empty[Json]
+      cfor(0)(_ < arr.length, _ + 1) { i =>
+        val x = arr(i)
+        jsons += encodeData(x, tColl.elemType)
       }
+      Json.fromValues(jsons.toList)
+    case t: STuple =>
+      val arr = Evaluation.fromDslTuple(v, t).asInstanceOf[t.WrappedType]
+      val tArr = t.items.toArray
+      val len = arr.length
+      assert(len == tArr.length, s"Type $t doesn't correspond to value $arr")
+      var obj = mutable.MutableList.empty[(String, Json)]
+      cfor(0)(_ < len, _ + 1) { i =>
+        obj += (s"_${i + 1}" -> encodeData[SType](arr(i),tArr(i)))
+      }
+      Json.fromFields(obj.toList)
+    case t => throw new SerializerException(s"Not defined DataSerializer for type $t")
   }
-//
-//  def encode[T <: SType](v: T#WrappedType, tpe: T): Json = {
-//    val encodedType = encodeSTypeName(tpe)
-//    val encodedData = encodeSTypeData(v, tpe)
-//    Json.obj(
-//      "type" -> Json.fromString(encodedType),
-//      "value" -> encodedData,
-//    )
-//  }
-//
-//  private def deserialize[T <: SType](tpe: T, json: Json): (T#WrappedType) = (tpe match {
-//    case SUnit => ()
-//    case SBoolean => r.getUByte() != 0
-//    case SByte => r.getByte()
-//    case SShort => r.getShort()
-//    case SInt => r.getInt()
-//    case SLong => r.getLong()
-//    case SString =>
-//      val size = r.getUInt().toInt
-//      val bytes = r.getBytes(size)
-//      new String(bytes, StandardCharsets.UTF_8)
-//    case SBigInt =>
-//      val size: Short = r.getUShort().toShort
-//      if (size > SBigInt.MaxSizeInBytes)
-//        throw new SerializerException(s"BigInt value doesn't not fit into ${SBigInt.MaxSizeInBytes} bytes: $size")
-//      val valueBytes = r.getBytes(size)
-//      SigmaDsl.BigInt(new BigInteger(valueBytes))
-//    case SGroupElement =>
-//      SigmaDsl.GroupElement(GroupElementSerializer.parse(r))
-//    case SSigmaProp =>
-//      SigmaDsl.SigmaProp(SigmaBoolean.serializer.parse(r))
-//    case SBox =>
-//      SigmaDsl.Box(ErgoBox.sigmaSerializer.parse(r))
-//    case SAvlTree =>
-//      SigmaDsl.avlTree(AvlTreeData.serializer.parse(r))
-//    case tColl: SCollectionType[a] =>
-//      val len = r.getUShort()
-//      if (tColl.elemType == SByte)
-//        Colls.fromArray(r.getBytes(len))
-//      else
-//        deserializeColl(len, tColl.elemType, r)
-//    case tuple: STuple =>
-//      val arr = tuple.items.map { t =>
-//        deserialize(t, r)
-//      }.toArray[Any]
-//      val coll = Colls.fromArray(arr)(RType.AnyType)
-//      Evaluation.toDslTuple(coll, tuple)
-//    case t =>
-//      CheckSerializableTypeCode(t.typeCode)
-//      throw new SerializerException(s"Not defined DataSerializer for type $t")
-//  }).asInstanceOf[T#WrappedType]
+
+  def decodeData[T <: SType](json: Json, tpe: T): (T#WrappedType) = {
+    val res = (tpe match {
+      case SUnit => Unit
+      case SBoolean => json.asBoolean.get
+      case SByte => json.asNumber.get.toByte.get
+      case SShort => json.asNumber.get.toShort.get
+      case SInt => json.asNumber.get.toInt.get
+      case SLong => json.asNumber.get.toLong.get
+      case SBigInt =>
+        val jsonBigInt = json.as[String]
+        jsonBigInt match {
+          case Right(jsonBigInt) =>
+            SigmaDsl.BigInt(new BigInteger(java.util.Base64.getDecoder.decode(jsonBigInt)))
+          case Left(error) => throw new SerializerException(error.getMessage)
+        }
+      case SString =>
+        val jsonBigInt = json.as[String]
+        jsonBigInt match {
+          case Right(jsonBigInt) =>
+            new String(java.util.Base64.getDecoder.decode(jsonBigInt))
+          case Left(error) => throw new SerializerException(error.getMessage)
+        }
+      case tColl: SCollectionType[a] =>
+        val tpeElem = tColl.elemType
+        decodeColl(json, tpeElem)
+      case t: STuple =>
+        val tArr = t.items.toArray
+        val collSource = mutable.ArrayBuilder.make[Any]()
+        cfor(1)(_ <= tArr.length, _ + 1) { i =>
+          collSource += decodeData(json.hcursor.downField(s"_${i}").focus.get, tArr(i - 1))
+        }
+        val coll = Colls.fromArray(collSource.result())(RType.AnyType)
+        Evaluation.toDslTuple(coll, t)
+    }).asInstanceOf[T#WrappedType]
+    res
+  }
+
+  private def decodeColl[T <: SType](json: Json, tpe: T): Coll[T#WrappedType] = {
+    implicit val tItem = (tpe match {
+      case tTup: STuple if tTup.items.length == 2 =>
+        Evaluation.stypeToRType(tpe)
+      case tTup: STuple =>
+        collRType(RType.AnyType)
+      case _ =>
+        Evaluation.stypeToRType(tpe)
+    }).asInstanceOf[RType[T#WrappedType]]
+    val jsonArray = json.as[List[Json]]
+    jsonArray match {
+      case Right(jsonArray) =>
+        val collSource = mutable.ArrayBuilder.make[T#WrappedType]()(tItem.classTag)
+        for (i <- jsonArray) {
+          collSource += decodeData(i, tpe)
+        }
+        Colls.fromArray(collSource.result())
+      case Left(error) => throw new SerializerException(error.getMessage)
+    }
+  }
+
+  def decode(json: Json): (SType#WrappedType)= {
+    val cursor: HCursor = json.hcursor
+    val tpe = SigmaParser.parseType(json.hcursor.downField("type").focus.get.asString.get)
+    val value = json.hcursor.downField("value").focus.get
+    val data = decodeData(value, tpe)
+    data
+  }
 }
--- src/test/scala/sigmastate/serialization/DataJsonEncoderSpecification.scala
+++ src/test/scala/sigmastate/serialization/DataJsonEncoderSpecification.scala
@@ -19,19 +19,13 @@ import SType.AnyOps
 import special.collection.Coll
 
 class DataJsonEncoderSpecification extends SerializationSpecification {
-//
-//  def roundtrip[T <: SType](obj: T#WrappedType, tpe: T) = {
-//    val w = SigmaSerializer.startWriter()
-//    DataSerializer.serialize(obj, tpe, w)
-//    val bytes = w.toBytes
-//    val r = SigmaSerializer.startReader(bytes, 0)
-//    val res = DataSerializer.deserialize(tpe, r)
-//    res shouldBe obj
-//    val randomPrefix = arrayGen[Byte].sample.get
-//    val r2 = SigmaSerializer.startReader(randomPrefix ++ bytes, randomPrefix.length)
-//    val res2 = DataSerializer.deserialize(tpe, r2)
-//    res2 shouldBe obj
-//  }
+
+  def roundtrip[T <: SType](obj: T#WrappedType, tpe: T) = {
+    val json = DataJsonEncoder.encode(obj, tpe)
+    val str = json.toString.replaceAll("\n", "").replaceAll(" ", "").strip()
+    val res = DataJsonEncoder.decode(json)
+    res shouldBe obj
+  }
 
   def testCollection[T <: SType](tpe: T) = {
     implicit val wWrapped = wrappedTypeGen(tpe)
@@ -39,59 +33,60 @@ class DataJsonEncoderSpecification extends SerializationSpecification {
     implicit val tagT = tT.classTag
     implicit val tAny = RType.AnyType
     forAll { xs: Array[T#WrappedType] =>
-//      println(DataJsonEncoder.encode[SCollection[T]](xs.toColl, SCollection(tpe)))
-//      roundtrip[SCollection[T]](xs.toColl, SCollection(tpe))
-//      roundtrip[SType](xs.toColl.map(x => (x, x)).asWrappedType, SCollection(STuple(tpe, tpe)))
-//
-//      val triples = xs.toColl.map(x => TupleColl(x, x, x)).asWrappedType
-//      roundtrip(triples, SCollection(STuple(tpe, tpe, tpe)))
-//
-//      val quartets = xs.toColl.map(x => TupleColl(x, x, x, x)).asWrappedType
-//      roundtrip(quartets, SCollection(STuple(tpe, tpe, tpe, tpe)))
-//
-//      val nested = xs.toColl.map(x => Colls.fromItems[T#WrappedType](x, x))
-//      roundtrip[SCollection[SCollection[T]]](nested, SCollection(SCollection(tpe)))
-//
-//      roundtrip[SType](
-//        xs.toColl.map { x =>
-//          val arr = Colls.fromItems[T#WrappedType](x, x)
-//          (arr, arr)
-//        }.asWrappedType,
-//        SCollection(STuple(SCollection(tpe), SCollection(tpe)))
-//      )
+      roundtrip[SCollection[T]](xs.toColl, SCollection(tpe))
+      roundtrip[SType](xs.toColl.map(x => (x, x)).asWrappedType, SCollection(STuple(tpe, tpe)))
+
+      val triples = xs.toColl.map(x => TupleColl(x, x, x)).asWrappedType
+      roundtrip(triples, SCollection(STuple(tpe, tpe, tpe)))
+
+      val quartets = xs.toColl.map(x => TupleColl(x, x, x, x)).asWrappedType
+      roundtrip(quartets, SCollection(STuple(tpe, tpe, tpe, tpe)))
+
+      val nested = xs.toColl.map(x => Colls.fromItems[T#WrappedType](x, x))
+      roundtrip[SCollection[SCollection[T]]](nested, SCollection(SCollection(tpe)))
+
+      roundtrip[SType](
+        xs.toColl.map { x =>
+          val arr = Colls.fromItems[T#WrappedType](x, x)
+          (arr, arr)
+        }.asWrappedType,
+        SCollection(STuple(SCollection(tpe), SCollection(tpe)))
+      )
     }
   }
-//
-//  def testTuples[T <: SType](tpe: T) = {
-//    implicit val wWrapped = wrappedTypeGen(tpe)
-//    implicit val tag = tpe.classTag[T#WrappedType]
-//    implicit val tAny = RType.AnyType
-//    forAll { in: (T#WrappedType, T#WrappedType) =>
-//      val (x,y) = (in._1, in._2)
-//      roundtrip[SType]((x, y).asWrappedType, STuple(tpe, tpe))
-//      roundtrip[SType](TupleColl(x, y, x).asWrappedType, STuple(tpe, tpe, tpe))
-//      roundtrip[SType](TupleColl(x, y, x, y).asWrappedType, STuple(tpe, tpe, tpe, tpe))
-//      roundtrip[STuple](Colls.fromItems[Any](x, y, (x, y)), STuple(tpe, tpe, STuple(tpe, tpe)))
-//      roundtrip[STuple](Colls.fromItems[Any](x, y, TupleColl(x, y, x)), STuple(tpe, tpe, STuple(tpe, tpe, tpe)))
-//      roundtrip[STuple](Colls.fromItems[Any](x, y, TupleColl(x, y, (x, y))), STuple(tpe, tpe, STuple(tpe, tpe, STuple(tpe, tpe))))
-//    }
-//  }
 
-  property("Data serialization round trip") {
+  def testTuples[T <: SType](tpe: T) = {
+    implicit val wWrapped = wrappedTypeGen(tpe)
+    implicit val tag = tpe.classTag[T#WrappedType]
+    implicit val tAny = RType.AnyType
+    forAll { in: (T#WrappedType, T#WrappedType) =>
+      val (x,y) = (in._1, in._2)
+      roundtrip[SType]((x, y).asWrappedType, STuple(tpe, tpe))
+      roundtrip[SType](TupleColl(x, y, x).asWrappedType, STuple(tpe, tpe, tpe))
+      roundtrip[SType](TupleColl(x, y, x, y).asWrappedType, STuple(tpe, tpe, tpe, tpe))
+      roundtrip[STuple](Colls.fromItems[Any](x, y, (x, y)), STuple(tpe, tpe, STuple(tpe, tpe)))
+      roundtrip[STuple](Colls.fromItems[Any](x, y, TupleColl(x, y, x)), STuple(tpe, tpe, STuple(tpe, tpe, tpe)))
+      roundtrip[STuple](Colls.fromItems[Any](x, y, TupleColl(x, y, (x, y))), STuple(tpe, tpe, STuple(tpe, tpe, STuple(tpe, tpe))))
+    }
+  }
 
-
-//    forAll { x: Byte => roundtrip[SByte.type](x, SByte) }
-//    forAll { x: Boolean => roundtrip[SBoolean.type](x, SBoolean) }
-//    forAll { x: Long => roundtrip[SLong.type](x, SLong) }
-//    forAll { x: String => roundtrip[SString.type](x, SString) }
-//    forAll { x: BigInteger => roundtrip[SBigInt.type](x, SBigInt) }
-//    forAll { x: EcPointType => roundtrip[SGroupElement.type](x, SGroupElement) }
-//    forAll { x: SigmaBoolean => roundtrip[SSigmaProp.type](x, SSigmaProp) }
-//    forAll { x: ErgoBox => roundtrip[SBox.type](x, SBox) }
-//    forAll { x: AvlTree => roundtrip[SAvlTree.type](x, SAvlTree) }
-//    forAll { x: Array[Byte] => roundtrip[SByteArray](x.toColl, SByteArray) }
-    forAll { t: SPredefType => testCollection(t) }
-//    testCollection(SBigInt)
+  property("Data Json serialization round trip") {
+    forAll { x: Byte => roundtrip[SByte.type](x, SByte) }
+    forAll { x: Boolean => roundtrip[SBoolean.type](x, SBoolean) }
+    forAll { x: Long => roundtrip[SLong.type](x, SLong) }
+    forAll { x: String => roundtrip[SString.type](x, SString) }
+    forAll { x: BigInteger => roundtrip[SBigInt.type](x, SBigInt) }
+    forAll { x: Array[Byte] => roundtrip[SByteArray](x.toColl, SByteArray) }
+    testCollection(SByte)
+    testCollection(SShort)
+    testCollection(SInt)
+    testCollection(SLong)
+    testCollection(SBigInt)
+    testTuples(SByte)
+    testTuples(SShort)
+    testTuples(SInt)
+    testTuples(SLong)
+    testTuples(SBigInt)
 //    forAll { t: SPredefType => testTuples(t) }
   }
 }
