29608245a2625b1284a1e364b97841020aa96e27
Denys Zadorozhnyi <denys@zadorozhnyi.com>
2019-08-29 17:12:43 +0300

    copy SigmaDsl.scala;

---
+++ contract-verification/src/main/scala/sigmastate/verification/SigmaDsl/Coll.scala
@@ -0,0 +1,28 @@
+package sigmastate.verification.SigmaDsl
+
+import sigmastate.verification.SigmaDsl.api.collection.Coll
+import stainless.annotation.{extern, pure}
+
+object Coll {
+
+  @extern @pure
+  def empty[A]: Coll[A] = ??? //new CollOverList(List())
+
+  @extern @pure
+  def apply[A](a: A): Coll[A] = ??? // new CollOverList(List(a))
+
+//  @library @extern
+//  def apply[A](list: List[A]): Coll[A] = ??? // new CollOverList(list)
+
+//  @library
+//  implicit class ListOps[A](val list: List[A]) {
+//    @extern
+//    def toColl: Coll[A] = Coll(list)
+//  }
+
+//  @library
+//  implicit class CollOps[A](val coll: Coll[A]) {
+//    @extern
+//    def toList: List[A] = ???
+//  }
+}
--- contract-verification/src/main/scala/sigmastate/verification/SigmaProp.scala
+++
@@ -1,9 +0,0 @@
-package sigmastate.verification
-
-import stainless.annotation.{extern, library, pure}
-
-@library
-case class SigmaProp(@extern sigmaProp: special.sigma.SigmaProp) {
-  @extern @pure
-  def isValid: Boolean = ???
-}
