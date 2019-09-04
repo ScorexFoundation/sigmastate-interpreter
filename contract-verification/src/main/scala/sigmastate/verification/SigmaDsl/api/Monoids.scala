d8558dd8c88559eba735f28595b2ab11418c95d4
Denys Zadorozhnyi <denys@zadorozhnyi.com>
2019-09-04 13:02:55 +0300

    make verifier happy and all green;

--- contract-verification/src/main/scala/sigmastate/verification/SigmaDsl/api/Monoids.scala
+++ contract-verification/src/main/scala/sigmastate/verification/SigmaDsl/api/Monoids.scala
@@ -1,19 +1,33 @@
 package sigmastate.verification.SigmaDsl.api
 
+import stainless.annotation.library
+
+@library
 trait Monoid[@specialized(Int, Long) T] {
+  @library
   def zero: T
+  @library
   def plus(x: T, y: T): T
+  @library
   def power(x: T, n: Int): T
 }
 
+@library
 trait MonoidBuilder {
+  @library
   def intPlusMonoid: Monoid[Int]
+  @library
   def intMaxMonoid: Monoid[Int]
+  @library
   def intMinMonoid: Monoid[Int]
 
+  @library
   def longPlusMonoid: Monoid[Long]
+  @library
   def longMaxMonoid: Monoid[Long]
+  @library
   def longMinMonoid: Monoid[Long]
+  @library
   def pairMonoid[@specialized(Int, Long) A, @specialized(Int, Long) B](m1: Monoid[A], m2: Monoid[B]): Monoid[(A, B)]
 
   //  def sizePlusMonoid[A](mA: Monoid[A]): Monoid[Size[A]]
