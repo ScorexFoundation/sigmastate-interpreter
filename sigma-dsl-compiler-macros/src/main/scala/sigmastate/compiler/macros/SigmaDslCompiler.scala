11af89e6c4bfb69b46ff543c080577c253a2e5d4
Denys Zadorozhnyi <denys@zadorozhnyi.com>
2019-10-23 10:44:28 +0300

    fix `compile` signature (introduce CompilationResult);

--- sigma-dsl-compiler-macros/src/main/scala/sigmastate/compiler/macros/SigmaDslCompiler.scala
+++ sigma-dsl-compiler-macros/src/main/scala/sigmastate/compiler/macros/SigmaDslCompiler.scala
@@ -1,14 +1,16 @@
 package sigmastate.compiler.macros
 
-import org.ergoplatform.dsl.ContractSyntax.Proposition
-import org.ergoplatform.dsl.PropositionSpec
+import sigmastate.Values.SigmaPropValue
 import sigmastate.compiler.macros.impl.SigmaDslCompilerImp
 import special.sigma.{Context, SigmaProp}
+import sigmastate.verification.SigmaDsl.api.sigma.{Context => VerifiedContext, SigmaProp => VerifiedSigmaProp}
 
 import scala.language.experimental.macros
 
+case class CompilationResult(ergoScala: Context => SigmaProp, root: SigmaPropValue)
+
 object SigmaDslCompiler {
 
-  def compile(contract: sigmastate.verification.SigmaDsl.api.sigma.Context => Boolean): (Context => Boolean, SigmaProp) = macro SigmaDslCompilerImp.compile
+  def compile(verifiedContract: VerifiedContext => VerifiedSigmaProp): CompilationResult = macro SigmaDslCompilerImp.compile
 
 }
