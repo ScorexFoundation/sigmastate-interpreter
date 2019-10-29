9e4424b62ada075a343a0fe3a283387e06d580e9
Alexander Slesarenko <avslesarenko@gmail.com>
2019-10-29 20:44:45 +0300

    optimized GetVarSerializer

--- sigmastate/src/main/scala/sigmastate/serialization/GetVarSerializer.scala
+++ sigmastate/src/main/scala/sigmastate/serialization/GetVarSerializer.scala
@@ -2,6 +2,7 @@ package sigmastate.serialization
 
 import sigmastate.Values._
 import sigmastate._
+import sigmastate.utils.SigmaByteWriter.DataInfo
 import sigmastate.utils.{SigmaByteReader, SigmaByteWriter}
 import sigmastate.utxo.GetVar
 
@@ -9,10 +10,11 @@ case class GetVarSerializer(cons: (Byte, SType) => Value[SOption[SType]])
   extends ValueSerializer[GetVar[_ <: SType]] {
   import sigmastate.Operations.GetVarInfo._
   override def opDesc = GetVar
+  val typeInfo: DataInfo[SType] = ArgInfo("type", "expected type of context variable")
 
   override def serialize(obj: GetVar[_ <: SType], w: SigmaByteWriter): Unit =
     w.put(obj.varId, varIdArg)
-      .putType(obj.tpe.elemType, ArgInfo("type", "expected type of context variable"))
+      .putType(obj.tpe.elemType, typeInfo)
 
   override def parse(r: SigmaByteReader): Value[SType] = {
     val varId = r.getByte()
