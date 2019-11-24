2203f2e6e7ec19ee9c101696a131a7094345fea2
Denys Zadorozhnyi <denys@zadorozhnyi.com>
2019-11-24 12:59:42 +0200

    move VerifiedTypeConvertersTest back under stainless;

--- contract-verification/src/test/scala/sigmastate/verification/test/VerifiedTypeConvertersTest.scala
+++ contract-verification/src/test/scala/sigmastate/verification/test/VerifiedTypeConvertersTest.scala
@@ -1,23 +1,22 @@
 package sigmastate.verification.test
 
 import org.scalacheck.Arbitrary.arbLong
-import org.scalacheck.Gen
-import scalan.RType
 import sigmastate.SCollection._
 import sigmastate.Values
 import sigmastate.Values.{ByteArrayConstant, ConcreteCollection}
 import sigmastate.eval.CSigmaProp
 import sigmastate.helpers.SigmaTestingCommons
-import sigmastate.serialization.generators.ObjectGenerators
 import sigmastate.verification.SigmaDsl.api.Iso
 import sigmastate.verification.SigmaDsl.api.VerifiedTypeConverters._
-import special.collection.{Coll, _}
+import special.collection._
+import stainless.annotation.ignore
 
+@ignore
 class VerifiedTypeConvertersTest extends SigmaTestingCommons with MiscGenerators {
 
   property("Coll[Coll[Byte]]") {
     forAll(collOfGen(byteCollGen(0, 10), 10)) { cc =>
-      Iso.roundTrip(cc) shouldEqual cc
+      assert(Iso.roundTrip(cc) == cc)
       val tree = VCollToErgoTree.to(cc)
       val expectedTree = ConcreteCollection(cc.toArray.map(ByteArrayConstant(_)) ,SByteArray)
       assert(tree == expectedTree)
@@ -26,7 +25,7 @@ class VerifiedTypeConvertersTest extends SigmaTestingCommons with MiscGenerators
 
   property("Coll[Coll[Long]]") {
     forAll(collOfGen(byteCollGen(0, 10).map(bs => bs.map(_.toLong)), 10)) { cc =>
-      Iso.roundTrip(cc) shouldEqual cc
+      assert(Iso.roundTrip(cc) == cc)
       val tree = VCollToErgoTree.to(cc)
       val expectedTree = ConcreteCollection(cc.toArray.map(Values.LongArrayConstant(_)), SLongArray)
       assert(tree == expectedTree)
@@ -35,19 +34,19 @@ class VerifiedTypeConvertersTest extends SigmaTestingCommons with MiscGenerators
 
   property("Coll[Coll[Coll[Byte]]]") {
     forAll(collOfGen(collOfGen(byteCollGen(0, 10), 10), 10)) { ccc =>
-      Iso.roundTrip(ccc) shouldEqual ccc
+      assert(Iso.roundTrip(ccc) == ccc)
     }
   }
 
   property("Coll[(Coll[Byte], Long)]") {
     forAll(collOfGen(byteCollGen(0, 10).map((_, arbLong.arbitrary.sample.get)), 10)) { tc =>
-      Iso.roundTrip(tc) shouldEqual tc
+      assert(Iso.roundTrip(tc) == tc)
     }
   }
 
   property("VSigmaPropToSigmaProp") {
     forAll(proveDlogGen.map(CSigmaProp)) { sp =>
-      Iso.roundTrip(sp) shouldEqual sp
+      assert(Iso.roundTrip(sp) == sp)
     }
   }
 }
