3f367a63dd9ac0c9d1a5034c9935da756285287d
Alexander Slesarenko <avslesarenko@gmail.com>
2020-06-19 14:20:05 +0300

    MethodCall resolution tests

--- sigmastate/src/test/scala/sigmastate/ErgoTreeSpecification.scala
+++ sigmastate/src/test/scala/sigmastate/ErgoTreeSpecification.scala
@@ -51,4 +51,70 @@ class ErgoTreeSpecification extends SigmaTestingData {
     }
   }
 
+  case class MInfo(methodId: Byte, method: SMethod, isSupported: Boolean = true)
+
+  // NOTE, the type code constants are checked above
+  // The methodId codes as checked here.
+  // The following table should be made dependent on HF activation
+  val methods = Table(
+    ("typeId",        "methods",               "CanHaveMethods"),
+    (SBoolean.typeId, Seq.empty[MInfo], true),
+    (SByte.typeId,    Seq.empty[MInfo], false),
+    (SShort.typeId,   Seq.empty[MInfo], false),
+    (SInt.typeId,     Seq.empty[MInfo], false),
+    (SLong.typeId,    Seq.empty[MInfo], false),
+    { import SNumericType._
+      (SBigInt.typeId,  Seq(
+        MInfo(methodId = 1, ToByteMethod, isSupported = false),
+        MInfo(2, ToShortMethod, isSupported = false),
+        MInfo(3, ToIntMethod, isSupported = false),
+        MInfo(4, ToLongMethod, isSupported = false),
+        MInfo(5, ToBigIntMethod, isSupported = false),
+        MInfo(6, ToBytesMethod, isSupported = false),
+        MInfo(7, ToBitsMethod, isSupported = false)
+      ), true)
+    },
+    { import SContext._
+      (SContext.typeId, Seq(
+        MInfo(1, dataInputsMethod), MInfo(2, headersMethod), MInfo(3, preHeaderMethod),
+        MInfo(4, inputsMethod), MInfo(5, outputsMethod), MInfo(6, heightMethod),
+        MInfo(7, selfMethod), MInfo(8, selfBoxIndexMethod), MInfo(9, lastBlockUtxoRootHashMethod),
+        MInfo(10, minerPubKeyMethod), MInfo(11, getVarMethod, isSupported = false)
+      ), true)
+    },
+    { import SGlobal._
+      (SGlobal.typeId, Seq(
+        MInfo(1, groupGeneratorMethod), MInfo(2, xorMethod)
+        ), true)
+    }
+//    (SNumericType.typeId, Seq.empty[(Int, SMethod)], true)
+  )
+
+  property("MethodCall Codes") {
+    forAll(methods) { (typeId, methods, shouldHaveMethods) =>
+      SType.types.get(typeId) match {
+        case Some(tyDesc) =>
+          assert(shouldHaveMethods, s"Type $tyDesc should NOT have methods")
+
+          tyDesc.methods.length shouldBe methods.length
+          for (expectedMethod <- methods) {
+            if (expectedMethod.isSupported) {
+
+              // the following line is used in MethodCall deserializer to resolve SMethod
+              val resolvedMethod = SMethod.fromIds(typeId, expectedMethod.methodId)
+
+              resolvedMethod.objType.typeId shouldBe typeId
+              resolvedMethod.name shouldBe expectedMethod.method.name
+              assert(resolvedMethod.irInfo.irBuilder.isDefined, s"irBuilder is not defined for $resolvedMethod")
+              resolvedMethod.irInfo shouldBe expectedMethod.method.irInfo
+            } else {
+              // declared, but not supported
+            }
+          }
+        case None =>
+          assert(!shouldHaveMethods, s"Type with code $typeId should have methods")
+      }
+    }
+
+  }
 }
