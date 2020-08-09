70d58154e6336229306de14ec4ae30dcbff791a6
Alexander Slesarenko <avslesarenko@gmail.com>
2020-08-09 18:19:59 +0300

    full coverage: TypesSpecification (part 2)

--- sigmastate/src/test/scala/sigmastate/TypesSpecification.scala
+++ sigmastate/src/test/scala/sigmastate/TypesSpecification.scala
@@ -15,13 +15,14 @@ class TypesSpecificatio extends SigmaTestingData {
 
   def Coll[T](items: T*)(implicit cT: RType[T]) = SigmaDsl.Colls.fromItems(items:_*)
 
+  def check(tpe: SType, v: Any, exp: Long) =
+    tpe.dataSize(v.asWrappedType) shouldBe exp
+
+  def checkColl[T](elemTpe: SType, arr: Coll[T], exp: Long) =
+    check(sigmastate.SCollection(elemTpe), arr, exp)
+
   property("SType.dataSize") {
-    def check(tpe: SType, v: Any, exp: Long) =
-      tpe.dataSize(v.asWrappedType) shouldBe exp
-
-    def checkColl[T](elemTpe: SType, arr: Coll[T], exp: Long) =
-      check(sigmastate.SCollection(elemTpe), arr, exp)
-
+    check(SUnit, (), 1)
     check(SBoolean, true, 1)
     check(SByte, 1.toByte, 1)
     check(SShort, 1.toShort, 2)
@@ -46,4 +47,23 @@ class TypesSpecificatio extends SigmaTestingData {
     check(STuple(SInt, STuple(SInt, SInt)), Array(10, Array[Any](20, 30)), 2 + 4 + (2 + 4 + 4))
   }
 
+  property("SType.dataSize (not defined)") {
+    assertExceptionThrown(check(NoType, null, 0), _.getMessage.contains("is not defined"))
+    assertExceptionThrown(check(SAny, null, 0), _.getMessage.contains("is not defined"))
+    assertExceptionThrown(check(STypeApply("T"), null, 0), _.getMessage.contains("is not defined"))
+    assertExceptionThrown(check(STypeVar("T"), null, 0), _.getMessage.contains("is not defined"))
+  }
+
+  property("SType.mkConstant (not defined)") {
+    val msg = "SType.mkConstant is not defined"
+    assertExceptionThrown({
+      val tpe = NoType.asInstanceOf[SType]
+      tpe.mkConstant(null.asInstanceOf[tpe.WrappedType])
+    }, { t =>
+        t.getMessage.contains(msg)
+    })
+    assertExceptionThrown(SAny.mkConstant(null), _.getMessage.contains(msg))
+    assertExceptionThrown(STypeApply("T").mkConstant(null), _.getMessage.contains(msg))
+    assertExceptionThrown(STypeVar("T").mkConstant(null), _.getMessage.contains(msg))
+  }
 }
