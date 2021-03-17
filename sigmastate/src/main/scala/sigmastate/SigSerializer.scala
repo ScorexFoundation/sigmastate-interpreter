4ec7ccf0516af4f460081c775bbb0665d7227bc1
Alexander Slesarenko <avslesarenko@gmail.com>
2021-03-18 00:23:37 +0300

    test-prove-verify: fix roundtrip property for SigSerializer

--- sigmastate/src/main/scala/sigmastate/SigSerializer.scala
+++ sigmastate/src/main/scala/sigmastate/SigSerializer.scala
@@ -137,7 +137,7 @@ object SigSerializer {
           }
 
           // Verifier doesn't need the polynomial anymore -- hence pass in None
-          CThresholdUncheckedNode(challenge, seq, t.k, None) -> (finalPos - pos)
+          CThresholdUncheckedNode(challenge, seq, t.k, Some(polynomial)) -> (finalPos - pos)
       }
     }
 
