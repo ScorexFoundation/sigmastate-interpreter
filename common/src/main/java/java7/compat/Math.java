4822340fca5d9f603cd364fd4e4d5bb2fe4004e7
Alexander Slesarenko <avslesarenko@gmail.com>
2021-09-16 11:57:53 +0300

    reduced-transaction: java7.compat.Math.toIntExact added

--- common/src/main/java/java7/compat/Math.java
+++ common/src/main/java/java7/compat/Math.java
@@ -156,4 +156,20 @@ public final class Math {
         }
         return r;
     }
+
+    /**
+     * Returns the value of the {@code long} argument;
+     * throwing an exception if the value overflows an {@code int}.
+     *
+     * @param value the long value
+     * @return the argument as an int
+     * @throws ArithmeticException if the {@code argument} overflows an int
+     * @since 1.8
+     */
+    public static int toIntExact(long value) {
+        if ((int)value != value) {
+            throw new ArithmeticException("integer overflow");
+        }
+        return (int)value;
+    }
 }
