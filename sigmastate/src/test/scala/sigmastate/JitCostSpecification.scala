4db92b6b21423758ca45fa65dd2a0eb4c62ccab1
Alexander Slesarenko <avslesarenko@gmail.com>
2021-12-09 02:49:24 +0300

    jitcost-type: fix tests (part 2)

--- sigmastate/src/test/scala/sigmastate/JitCostSpecification.scala
+++ sigmastate/src/test/scala/sigmastate/JitCostSpecification.scala
@@ -9,9 +9,7 @@ class JitCostSpecification extends SigmaTestingData {
   type BinOp = (Int, Int) => Int
   type JitBinOp = (JitCost, JitCost) => JitCost
 
-  def testBinary(x: Int, y: Int, f: BinOp, jitf: JitBinOp) = {
-    val r1 = Try(f(x, y))
-    val r2 = Try(jitf(new JitCost(x), new JitCost(y)).value)
+  def testBinary[A](r1: Try[A], r2: Try[A]) = {
     (r1, r2) match {
       case (Failure(exception), Failure(expectedException)) =>
         rootCause(exception).getClass shouldBe expectedException.getClass
@@ -22,7 +20,36 @@ class JitCostSpecification extends SigmaTestingData {
 
   property("JitCost.$plus") {
     forAll(MinSuccessful(1000)) { (x: Int, y: Int) =>
-      testBinary(x, y, java7.compat.Math.addExact, (c1, c2) => c1 + c2)
+      {
+        val r1 = Try(java7.compat.Math.addExact(x, y))
+        val r2 = Try((JitCost(x) + JitCost(y)).value)
+        testBinary(r1, r2)
+      }
+
+      {
+        val r1 = Try(java7.compat.Math.multiplyExact(x, y))
+        val r2 = Try((JitCost(x) * y).value)
+        testBinary(r1, r2)
+      }
+
+      {
+        val r1 = Try(x / y)
+        val r2 = Try((JitCost(x) / y).value)
+        testBinary(r1, r2)
+      }
+
+      {
+        val r1 = Try(x > y)
+        val r2 = Try(JitCost(x) > JitCost(y))
+        testBinary(r1, r2)
+      }
+
+      {
+        val r1 = Try(x >= y)
+        val r2 = Try(JitCost(x) >= JitCost(y))
+        testBinary(r1, r2)
+      }
+
     }
   }
 
