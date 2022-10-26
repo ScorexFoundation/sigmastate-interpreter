37397c9f961ecf4485eb07fa03cd4f5b996c02ee
Alexander Slesarenko <avslesarenko@gmail.com>
2022-10-26 17:27:32 +0200

    v5.x-scala-js: importing sigmajs-crypto-facade (part 3)

--- interpreter/js/src/main/scala/sigmastate/crypto/Imported.scala
+++ interpreter/js/src/main/scala/sigmastate/crypto/Imported.scala
@@ -2,6 +2,7 @@ package sigmastate.crypto
 
 import scala.scalajs.js
 import scala.scalajs.js.annotation.JSImport
+import scala.scalajs.js.typedarray.Uint8Array
 
 @js.native
 @JSImport("sigmajs-crypto-facade", "CryptoContext")
@@ -14,3 +15,46 @@ class CryptoContextJs() extends js.Object {
   def getGenerator(): Platform.Ecp = js.native
 }
 
+@js.native
+@JSImport("sigmajs-crypto-facade", "CryptoFacade")
+object CryptoFacadeJs extends js.Object {
+  def normalizePoint(point: Platform.Ecp): Platform.Ecp = js.native
+
+  def createCryptoContext(): CryptoContextJs = js.native
+
+  def negatePoint(point: Platform.Ecp): Platform.Ecp = js.native 
+
+  def isInfinityPoint(point: Platform.Ecp): Boolean = js.native 
+
+  def multiplyPoint(point: Platform.Ecp, scalar: js.BigInt): Platform.Ecp = js.native
+
+  def addPoint(point1: Platform.Ecp, point2: Platform.Ecp): Platform.Ecp = js.native
+
+  def showPoint(point: Platform.Ecp): String = js.native
+
+  def testBitZeroOfFieldElem(element: js.BigInt): Boolean = js.native
+
+  def getEncodedOfFieldElem(element: js.BigInt): Uint8Array = js.native
+
+  def getXCoord(point: Platform.Ecp): js.BigInt = js.native
+
+  def getYCoord(point: Platform.Ecp): js.BigInt = js.native
+
+  def getAffineXCoord(point: Platform.Ecp): js.BigInt = js.native
+
+  def getAffineYCoord(point: Platform.Ecp): js.BigInt = js.native
+}
+
+@js.native
+@JSImport("@noble/secp256k1", "Point")
+object Point extends js.Any {
+  def fromHex(hex: String): Platform.Ecp = js.native
+  def ZERO: Platform.Ecp = js.native
+}
+
+@js.native
+@JSImport("@noble/secp256k1", "utils")
+object utils extends js.Any {
+  def bytesToHex(bytes: Uint8Array): String = js.native
+  def hexToBytes(hex: String): Uint8Array = js.native
+}
