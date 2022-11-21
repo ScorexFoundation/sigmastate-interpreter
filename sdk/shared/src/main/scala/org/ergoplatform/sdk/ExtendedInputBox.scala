bc79492463a6f3bc737a6749a8e41b13c6b00a91
Alexander Slesarenko <avslesarenko@gmail.com>
2022-11-21 20:14:13 +0100

    v5.x-scala-js: to sdk module: added ExtendedInputBox.scala

---
+++ sdk/shared/src/main/scala/org/ergoplatform/sdk/ExtendedInputBox.scala
@@ -0,0 +1,19 @@
+package org.ergoplatform.sdk
+
+import org.ergoplatform.{ErgoBox, UnsignedInput}
+import sigmastate.interpreter.ContextExtension
+
+/** Input ErgoBox paired with context variables (aka ContextExtensions).
+ *
+ * @param box       an instance of ErgoBox which is used as an input of the transaction.
+ * @param extension a set of context variables necessary to satisfy the box's
+ *                  guarding proposition.
+ *                  This extension is also saved in the corresponding
+ *                  [[org.ergoplatform.Input]] instance of the signed transaction.
+ */
+case class ExtendedInputBox(
+  box: ErgoBox,
+  extension: ContextExtension
+) {
+  def toUnsignedInput: UnsignedInput = new UnsignedInput(box.id, extension)
+}
