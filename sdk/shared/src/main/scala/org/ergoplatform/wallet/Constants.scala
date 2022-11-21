623126802308885a612365d69296dfbce69d1aed
Alexander Slesarenko <avslesarenko@gmail.com>
2022-11-21 19:56:15 +0100

    v5.x-scala-js: to sdk module: added DerivationPath.scala, ExtendedKey.scala

--- sdk/shared/src/main/scala/org/ergoplatform/wallet/Constants.scala
+++ sdk/shared/src/main/scala/org/ergoplatform/wallet/Constants.scala
@@ -1,5 +1,20 @@
 package org.ergoplatform.wallet
 
+import org.ergoplatform.wallet.secrets.DerivationPath
+
 object Constants {
+  /** part of the protocol, do not change */
+  val SecretKeyLength = 32
+
   val Encoding = "UTF-8"
+
+  /**
+    * Pre - EIP3 derivation path
+    */
+  val preEip3DerivationPath: DerivationPath = DerivationPath.fromEncoded("m/1").get
+
+  /**
+    * Post - EIP3 derivation path
+    */
+  val eip3DerivationPath: DerivationPath = DerivationPath.fromEncoded("m/44'/429'/0'/0/0").get
 }
