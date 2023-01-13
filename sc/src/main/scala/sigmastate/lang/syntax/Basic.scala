9270d42c36b83c53b54748caffc9ec09a742acb5
Alexander Slesarenko <avslesarenko@gmail.com>
2023-01-13 11:01:59 +0100

    v5.x-scala-js: ScalaDocs for sigmastate.lang.syntax.Basic

--- sc/src/main/scala/sigmastate/lang/syntax/Basic.scala
+++ sc/src/main/scala/sigmastate/lang/syntax/Basic.scala
@@ -7,40 +7,59 @@ import scalan.Nullable
 import sigmastate.lang.SourceContext
 import sigmastate.exceptions.CompilerException
 
+/** Basic lexical parsers for ErgoScript. */
 object Basic {
   val digits = "0123456789"
-  def Digit[_:P]: P[Unit] = P( CharPred(digits.contains(_)) )
   val hexDigits: String = digits + "abcdefABCDEF"
+
+  /** Matches a decimal digit. */
+  def Digit[_:P]: P[Unit] = P( CharPred(digits.contains(_)) )
+  /** Matches a hexadecimal digit. */
   def HexDigit[_:P]: P[Unit] = P( CharPred(hexDigits.contains(_)) )
+
   def UnicodeEscape[_:P]: P[Unit] = P( "u" ~ HexDigit ~ HexDigit ~ HexDigit ~ HexDigit )
 
   //Numbers and digits
+  /** Matches a positive hexadecimal number. */
   def HexNum[_:P]: P[Unit] = P( "0x" ~ CharsWhile(hexDigits.contains(_), 1) )
+  /** Matches a positive decimal number. */
   def DecNum[_:P]: P[Unit] = P( CharsWhile(digits.contains(_), 1) )
+  /** Matches a exponent part of floating point number. */
   def Exp[_:P]: P[Unit] = P( CharPred("Ee".contains(_)) ~ CharPred("+-".contains(_)).? ~ DecNum )
+
   def FloatType[_:P]: P[Unit] = P( CharIn("fFdD") )
 
+  /** Matches a sequience of whitespace character. */
   def WSChars[_:P]: P[Unit] = P( CharsWhileIn("\u0020\u0009") )
   def Newline[_:P]: P[Unit] = P( StringIn("\r\n", "\n") )
   def Semi[_:P]: P[Unit] = P( ";" | Newline.rep(1) )
+
+  /** Matches a single operation character. */
   def OpChar[_:P]: P[Unit] = P ( CharPred(isOpChar) )
 
+  /** Characters allowed in as operations. */
   def isOpChar(c: Char): Boolean = c match{
     case '!' | '#' | '%' | '&' | '*' | '+' | '-' | '/' |
          ':' | '<' | '=' | '>' | '?' | '@' | '\\' | '^' | '|' | '~' => true
     case _ => isOtherSymbol(c) || isMathSymbol(c)
   }
-  def Letter[_:P]: P[Unit] = P( CharPred(c => isLetter(c) | isDigit(c) | c == '$' | c == '_' ) )
+
+  /** Character allowed in identificators. */
   def LetterDigitDollarUnderscore[_:P]: P[Unit] =  P(
     CharPred(c => isLetter(c) | isDigit(c) | c == '$' | c == '_' )
   )
+  /** Lower-case letters. */
   def Lower[_:P]: P[Unit] = P( CharPred(c => isLower(c) || c == '$' | c == '_') )
+  /** Upper-case letters. */
   def Upper[_:P]: P[Unit] = P( CharPred(isUpper) )
 
   def error(msg: String, srcCtx: Option[SourceContext]) = throw new ParserException(msg, srcCtx)
   def error(msg: String, srcCtx: Nullable[SourceContext]) = throw new ParserException(msg, srcCtx.toOption)
 }
 
+/** @param message error message
+  * @param source  descriptor of source context of the error
+  */
 class ParserException(message: String, source: Option[SourceContext])
   extends CompilerException(message, source)
 
