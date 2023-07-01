04ce57092243ede3647b02d1bb77b0868e0a9069
Alexander Slesarenko <avslesarenko@gmail.com>
2023-07-01 18:45:14 +0200

    compiler-js: more sc.spec.js tests

--- sigma-js/tests/js/sc.spec.js
+++ sigma-js/tests/js/sc.spec.js
@@ -1,4 +1,4 @@
-const { Types, Values, ErgoTree, SigmaCompiler, SigmaCompilerObj } = require("sigmastate-js");
+const { TypeObj, ValueObj, ErgoTree, SigmaCompiler, SigmaCompilerObj } = require("sigmastate-js");
 
 describe("Smoke tests for API exporting", () => {
   let compiler = SigmaCompilerObj.forMainnet();
@@ -7,13 +7,24 @@ describe("Smoke tests for API exporting", () => {
     expect(compiler).not.toBeUndefined();
   });
 
+  let segregatedTreeHex = "100104c801d191a37300";
+  let embedddedTreeHex = "00d191a304c801";
+
   it("SigmaCompiler should compile", () => {
     let treeWithSegregation = compiler.compile({}, true, 0, "sigmaProp(HEIGHT > 100)");
     expect(treeWithSegregation).not.toBeUndefined();
-    expect(treeWithSegregation.toHex()).toEqual("100104c801d191a37300")
+    expect(treeWithSegregation.toHex()).toEqual(segregatedTreeHex)
 
     let treeWithoutSegregation = compiler.compile({}, false, 0, "sigmaProp(HEIGHT > 100)");
-    expect(treeWithoutSegregation.toHex()).toEqual("00d191a304c801")
+    expect(treeWithoutSegregation.toHex()).toEqual(embedddedTreeHex)
+  });
+
+  it("SigmaCompiler should compile with named constants", () => {
+    let treeWithSegregation = compiler.compile(
+        {"deadline": ValueObj.ofInt(100)},
+        true, 0, "sigmaProp(HEIGHT > deadline)");
+    expect(treeWithSegregation).not.toBeUndefined();
+    expect(treeWithSegregation.toHex()).toEqual(segregatedTreeHex)
   });
 });
 
