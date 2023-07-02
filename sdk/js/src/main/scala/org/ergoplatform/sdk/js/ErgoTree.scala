79df4ca171a77233947d835042ce5c82ee520469
Alexander Slesarenko <avslesarenko@gmail.com>
2023-07-02 17:02:20 +0200

    compiler-js: added properties to js.ErgoTree

--- sdk/js/src/main/scala/org/ergoplatform/sdk/js/ErgoTree.scala
+++ sdk/js/src/main/scala/org/ergoplatform/sdk/js/ErgoTree.scala
@@ -5,20 +5,32 @@ import sigmastate.Values
 import sigmastate.serialization.ErgoTreeSerializer
 
 import scala.scalajs.js
-import scala.scalajs.js.annotation.{JSExportTopLevel}
+import scala.scalajs.js.JSConverters.JSRichIterableOnce
+import scala.scalajs.js.annotation.JSExportTopLevel
 
 /** An exported JavaScript class wrapping the Scala `Values.ErgoTree` type. */
 @JSExportTopLevel("ErgoTree")
 class ErgoTree(tree: Values.ErgoTree) extends js.Object {
+  /** The first byte of serialized byte array which determines interpretation of the rest of the array. */
+  def header(): Byte = tree.header
+
+  def version(): Byte = tree.version
+
+  def isConstantSegregation(): Boolean = tree.isConstantSegregation
+
+  def hasSize(): Boolean = tree.hasSize
+
   /** Serializes the ErgoTree instance to an array of bytes. */
-  def toBytes(): Array[Byte] = {
-    val bytes = ErgoTreeSerializer.DefaultSerializer.serializeErgoTree(tree)
-    bytes
-  }
+  def bytes(): Array[Byte] = tree.bytes
 
   /** Serializes the ErgoTree instance to a hexadecimal string. */
-  def toHex(): String = {
-    Base16.encode(toBytes())
+  def toHex(): String = tree.bytesHex
+
+  /** Returns segregated constants of this tree as [[js.Array]]. */
+  def constants(): js.Array[Value] = {
+    val constants = tree.constants
+    val values = constants.map(Isos.isoValueToConstant.from)
+    values.toJSArray
   }
 }
 
