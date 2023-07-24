ec0d671b3fc5209973fd45bd66394a5d94e168e0
arobsn <alissonrobson@proton.me>
2023-07-24 16:23:25 -0300

    compile to using fullOpt

--- sigma-js/jest.config.js
+++ sigma-js/jest.config.js
@@ -3,8 +3,7 @@ const config = {
   transform: {}, // reduce non-cached test time by about 20x by disabling babel code transformation
   moduleDirectories: ["<rootDir>/node_modules"],
   moduleNameMapper: {
-    "sigmastate-js/main":
-      "<rootDir>/../sc/js/target/scala-2.13/sc-fastopt/main.js",
+    "sigmastate-js/main": "<rootDir>/../sc/js/target/scala-2.13/sc-opt/main.js",
   },
 };
 
