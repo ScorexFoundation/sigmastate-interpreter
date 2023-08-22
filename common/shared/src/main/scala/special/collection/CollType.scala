0e979b6f7a2ca55d442c4e13a11ed6d3c976df58
Alexander Slesarenko <avslesarenko@gmail.com>
2023-08-22 16:38:28 +0200

    minimize-modules: move downcastCollType to CollType

--- common/shared/src/main/scala/special/collection/CollType.scala
+++ common/shared/src/main/scala/special/collection/CollType.scala
@@ -1,6 +1,8 @@
 package special.collection
 
 import scalan.RType
+
+import scala.language.implicitConversions
 import scala.reflect.ClassTag
 
 /** Type descriptor for `Coll[A]` type. */
@@ -8,4 +10,15 @@ case class CollType[A](tItem: RType[A]) extends RType[Coll[A]] {
   override val classTag: ClassTag[Coll[A]] = ClassTag[Coll[A]](classOf[Coll[A]])
 
   override def name: String = s"Coll[${tItem.name}]"
+}
+
+object CollType {
+  /** Conversion to underlying descriptor class.
+    * Allows syntax like
+    *
+    * ```val tColl: RType[Coll[A]] = ...; tColl.tItem```
+    *
+    * where `tItem` is a method of `CollType`, but is not defined on `RType`.
+    */
+  implicit def downcastCollType[A](ct: RType[Coll[A]]): CollType[A] = ct.asInstanceOf[CollType[A]]
 }
--- common/shared/src/main/scala/special/collection/package.scala
+++ common/shared/src/main/scala/special/collection/package.scala
@@ -12,15 +12,6 @@ package object collection {
   /** Implicit resolution of `Coll[A]` type descriptor, given a descriptor of `A`. */
   implicit def collRType[A](implicit tA: RType[A]): RType[Coll[A]] = CollType[A](tA)
 
-  /** Conversion to underlying descriptor class.
-    * Allows syntax like
-    *
-    * ```val tColl: RType[Coll[A]] = ...; tColl.tItem```
-    *
-    * where `tItem` is a method of `CollType`, but is not defined on `RType`.
-    */
-  implicit def downcastCollType[A](ct: RType[Coll[A]]): CollType[A] = ct.asInstanceOf[CollType[A]]
-
   implicit val collBuilderRType: RType[CollBuilder] = RType.fromClassTag(classTag[CollBuilder])
 
   private def sameLengthErrorMsg[A, B](xs: Coll[A], ys: Coll[B]) =
