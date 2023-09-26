dca61811bd8dc839b97178d9c862d65c210c3af2
Alexander Slesarenko <avslesarenko@gmail.com>
2023-09-26 22:38:21 +0200

    v5.0.11-fix1: abstract classes for BlockchainParameters and BlockchainStateContext

--- sdk/shared/src/main/scala/org/ergoplatform/sdk/wallet/protocol/context/BlockchainStateContext.scala
+++ sdk/shared/src/main/scala/org/ergoplatform/sdk/wallet/protocol/context/BlockchainStateContext.scala
@@ -5,16 +5,16 @@ import special.collection.Coll
 /** Blockchain context used in tx signing. */
 abstract class BlockchainStateContext {
   /** Fixed number (10 in Ergo) of last block headers. */
-  def sigmaLastHeaders: Coll[sigma.Header]
+  def sigmaLastHeaders: Coll[special.sigma.Header]
   /** UTXO set digest from a last header (of sigmaLastHeaders). */
   def previousStateDigest: Coll[Byte]
   /** Returns pre-header (header without certain fields) of the current block. */
-  def sigmaPreHeader: sigma.PreHeader
+  def sigmaPreHeader: special.sigma.PreHeader
 }
 
 /** Blockchain context used in tx signing. */
 case class CBlockchainStateContext(
-  sigmaLastHeaders: Coll[sigma.Header],
+  sigmaLastHeaders: Coll[special.sigma.Header],
   previousStateDigest: Coll[Byte],
-  sigmaPreHeader: sigma.PreHeader
+  sigmaPreHeader: special.sigma.PreHeader
 ) extends BlockchainStateContext
