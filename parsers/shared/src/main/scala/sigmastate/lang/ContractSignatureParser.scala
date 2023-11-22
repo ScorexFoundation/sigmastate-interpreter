b3df52137e2fcd331a7110fa606d8f426f01e4b0
ross-weir <29697678+ross-weir@users.noreply.github.com>
2023-11-22 19:57:20 +1100

    impl ContractSignature parser

---
+++ parsers/shared/src/main/scala/sigmastate/lang/ContractSignatureParser.scala
@@ -0,0 +1,28 @@
+package sigmastate.lang
+
+import fastparse._
+import fastparse.NoWhitespace._
+import sigmastate.SType
+import sigmastate.Values.Constant
+
+case class ContractParam(name: String, tpe: SType, defaultValue: Option[Constant[_]])
+
+case class ContractSignature(name: String, params: Seq[ContractParam])
+
+object ContractSignatureParser {
+  import SigmaParser._
+
+  def parse(source: String): Parsed[ContractSignature] = {
+    fastparse.parse(source, parse(_))
+  }
+
+  def parse[_: P]: P[ContractSignature] = P(annotation ~ " ".rep.? ~ `def` ~ " ".rep.? ~ Id.! ~ params).map(s => ContractSignature(s._1, s._2.getOrElse(Seq())))
+
+  private def annotation[_: P] = P("@contract")
+
+  private def paramDefault[_: P] = P(" ".rep.? ~ `=` ~ " ".rep.? ~ ExprLiteral)
+
+  private def param[_: P] = P(" ".rep.? ~ Id.! ~ ":" ~ Type ~ paramDefault.?).map(s => ContractParam(s._1, s._2, s._3))
+
+  private def params[_: P] = P("(" ~ param.rep(1, ",").? ~ ")")
+}
