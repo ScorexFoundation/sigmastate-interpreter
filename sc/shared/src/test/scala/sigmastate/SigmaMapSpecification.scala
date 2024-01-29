1fea1e2f339dae5ceab92beb254c161f5f770eec
Alexander Chepurnoy <alex.chepurnoy@iohk.io>
2024-01-29 17:55:25 +0300

    traversal vectors test

--- sc/shared/src/test/scala/sigmastate/SigmaMapSpecification.scala
+++ sc/shared/src/test/scala/sigmastate/SigmaMapSpecification.scala
@@ -15,6 +15,20 @@ class SigmaMapSpecification extends TestingCommons {
     empty.iterator.toSeq.isEmpty shouldBe true
   }
 
+  property("traversal vectors") {
+      val sm = SigmaMap(Map(
+        0.toByte -> IntConstant(0),
+        1.toByte -> IntConstant(1),
+        2.toByte -> IntConstant(2),
+        3.toByte -> IntConstant(3),
+        4.toByte -> IntConstant(4)
+      ))
+
+    sm.maxKey shouldBe 4
+    sm.knownSize shouldBe 5
+    sm.iterator.toList.map(_._1) shouldBe Array[Byte](0, 1, 2, 3, 4)
+  }
+
   property("traversal equivalence") {
     val fullArr = (0 to Byte.MaxValue).map { b => b.toByte -> IntConstant(b) }
     forAll(Gen.chooseNum(0, Byte.MaxValue)) { n =>
