ff94bd13d3f9636f1b25d49a58acef728f23eb6e
Alexander Chepurnoy <alex.chepurnoy@iohk.io>
2024-01-29 17:49:30 +0300

    traversal equivalence test

--- sc/shared/src/test/scala/sigmastate/SigmaMapSpecification.scala
+++ sc/shared/src/test/scala/sigmastate/SigmaMapSpecification.scala
@@ -1,23 +1,29 @@
 package sigmastate
 
 import org.scalacheck.Gen
+import sigma.ast.{EvaluatedValue, IntConstant, SType}
 import sigma.interpreter.SigmaMap
 import sigmastate.helpers.TestingCommons
 
-// old representation scala.collection.Map[Byte, EvaluatedValue[_ <: SType]]
+import scala.util.Random
+
 class SigmaMapSpecification extends TestingCommons {
 
-  property ("SigmaMap.empty") {
+  property("SigmaMap.empty") {
     val empty = SigmaMap.empty
     empty.knownSize shouldBe 0
     empty.iterator.toSeq.isEmpty shouldBe true
   }
 
   property("traversal equivalence") {
-    forAll(Gen.chooseNum(0, Byte.MaxValue)){ n =>
-     // (0 until n) do {
+    val fullArr = (0 to Byte.MaxValue).map { b => b.toByte -> IntConstant(b) }
+    forAll(Gen.chooseNum(0, Byte.MaxValue)) { n =>
+      val rnd = Random.shuffle(fullArr).take(n)
+      val oldRepr: scala.collection.Map[Byte, EvaluatedValue[_ <: SType]] = rnd.toMap
+      val sm = SigmaMap(oldRepr)
 
-     // }
+      oldRepr.iterator.toList shouldBe sm.iterator.toList
     }
   }
+
 }
