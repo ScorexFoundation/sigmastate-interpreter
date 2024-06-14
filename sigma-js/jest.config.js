a4ab08fbcfb78f99cf18914b963518d0f493ae7b
c8e4 <c8e4@proton.me>
2024-06-14 18:06:00 +0200

    Revert "remove rootDir prefix"
    This reverts commit 38841c80ec4174fe2dfe3838d0f50f479e3dda4f.

--- sigma-js/jest.config.js
+++ sigma-js/jest.config.js
@@ -1,7 +1,7 @@
 /** @type {import("jest").Config} */
 const config = {
   transform: {}, // reduce non-cached test time by about 20x by disabling babel code transformation
-  moduleDirectories: ["node_modules"],
+  moduleDirectories: ["<rootDir>/node_modules"],
   moduleNameMapper: {
     "sigmastate-js/main":
       "<rootDir>/../sc/js/target/scala-2.13/sc-fastopt/main.js",
