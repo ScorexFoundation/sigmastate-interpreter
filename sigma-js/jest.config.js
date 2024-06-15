92b02a75546688aab0a25b25db458e2aae01171d
c8e4 <c8e4@proton.me>
2024-06-15 08:13:31 +0200

    fix whitespace

--- sigma-js/jest.config.js
+++ sigma-js/jest.config.js
@@ -3,10 +3,9 @@ const config = {
 	transform: {}, // reduce non-cached test time by about 20x by disabling babel code transformation
 	moduleDirectories: ["<rootDir>/node_modules"],
 	moduleNameMapper: {
-	  "sigmastate-js/main":
-		"<rootDir>/../sc/js/target/scala-2.13/sc-fastopt/main.js",
+		"sigmastate-js/main":
+			"<rootDir>/../sc/js/target/scala-2.13/sc-fastopt/main.js",
 	},
-  };
-  
-  module.exports = config;
-  
+};
+
+module.exports = config;
