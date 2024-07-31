ba878b8820e157af17921a4e600499bc8f478f1c
Alexander Chepurnoy <alex.chepurnoy@iohk.io>
2024-07-31 22:51:26 +0300

    fixing GraphIRReflection for checkPow, test for isoHeader for known vector

--- data/shared/src/main/scala/sigma/ast/methods.scala
+++ data/shared/src/main/scala/sigma/ast/methods.scala
@@ -1460,7 +1460,7 @@ case object SHeaderMethods extends MonoTypeMethods {
   lazy val checkPowMethod = SMethod(
     this, "checkPow", SFunc(Array(SHeader), SBoolean), 16, FixedCost(JitCost(700)))
     .withIRInfo(MethodCallIrBuilder)
-    .withInfo("Validate header's proof-of-work")
+    .withInfo(MethodCall, "Validate header's proof-of-work")
 
   def checkPow_eval(mc: MethodCall, G: SigmaDslBuilder, header: Header)
                  (implicit E: ErgoTreeEvaluator): Boolean = {
